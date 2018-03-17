d2b70f663f24f362ea500c54cbb1794e68131f22
James Eagle <jimeagle@gmail.com>
2018-03-17 17:03:38 +0000

    Tidy news

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,5 +1,13 @@
-$('select.form-control-chosen').chosen({
-    disable_search_threshold: 10,
-    allow_single_deselect: true,
-    rtl: false
-});
+if ($('#apps-page').length > 0) {
+
+    $('select.form-control-chosen').chosen({
+        disable_search_threshold: 10,
+        allow_single_deselect: true,
+        rtl: false
+    });
+}
+
+if ($('#app-page').length > 0) {
+
+    $('.collapse').collapse();
+}
