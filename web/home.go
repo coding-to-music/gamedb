c7c6813a4d1168a35c2955928f1810d88ec5a596
James Eagle <james.eagle@fortifi.it>
2018-04-25 13:04:22 +0100

    Redo home

--- web/home.go
+++ web/home.go
@@ -14,32 +14,6 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
 	var err error
 
-	var playersCount int
-	wg.Add(1)
-	go func() {
-
-		playersCount, err = datastore.CountPlayers()
-		if err != nil {
-			logger.Error(err)
-		}
-
-		wg.Done()
-
-	}()
-
-	//var pricesCount int
-	//wg.Add(1)
-	//go func() {
-	//
-	//	pricesCount, err = datastore.CountPrices()
-	//	if err != nil {
-	//		logger.Error(err)
-	//	}
-	//
-	//	wg.Done()
-	//
-	//}()
-
 	var ranksCount int
 	wg.Add(1)
 	go func() {
@@ -66,23 +40,34 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	}()
 
+	var packagesCount int
+	wg.Add(1)
+	go func() {
+
+		packagesCount, err = mysql.CountPackages()
+		if err != nil {
+			logger.Error(err)
+		}
+
+		wg.Done()
+
+	}()
+
 	wg.Wait()
 
 	template := homeTemplate{}
 	template.Fill(r, "Home")
 
-	template.PlayersCount = playersCount
-	//template.PricesCount = pricesCount
 	template.RanksCount = ranksCount
 	template.AppsCount = appsCount
+	template.PackagesCount = packagesCount
 
 	returnTemplate(w, r, "home", template)
 }
 
 type homeTemplate struct {
 	GlobalTemplate
-	PlayersCount int
-	PricesCount  int
-	RanksCount   int
-	AppsCount    int
+	RanksCount    int
+	AppsCount     int
+	PackagesCount int
 }
