4f2b69f10926f3c0d15fb335c70ffe67a1dcc08d
James Eagle <jimeagle@gmail.com>
2018-10-03 21:05:07 +0100

    Remove old changes code

--- web/changes.go
+++ web/changes.go
@@ -2,108 +2,13 @@ package web
 
 import (
 	"net/http"
-	"sync"
 
 	"cloud.google.com/go/datastore"
 	"github.com/steam-authority/steam-authority/db"
 	"github.com/steam-authority/steam-authority/logger"
-	"github.com/steam-authority/steam-authority/structs"
-)
-
-const (
-	changesLimit = 100
 )
 
 func ChangesHandler(w http.ResponseWriter, r *http.Request) {
-
-	// Get changes
-	var changes []structs.ChangesChangeTemplate
-	resp, err := db.GetLatestChanges(changesLimit, 1)
-	logger.Error(err)
-
-	for _, v := range resp {
-		changes = append(changes, structs.ChangesChangeTemplate{
-			Change: v,
-		})
-	}
-
-	//
-	var wg = sync.WaitGroup{}
-
-	// Get apps
-	wg.Add(1)
-	go func() {
-
-		// Get app IDs
-		var appIDs []int
-		for _, v := range changes {
-			appIDs = append(appIDs, v.Change.GetAppIDs()...)
-		}
-
-		// Get apps for all changes
-		appsMap := make(map[int]db.App)
-		apps, err := db.GetApps(appIDs, []string{"id", "name", "icon"})
-		logger.Error(err)
-
-		// Make app map
-		for _, v := range apps {
-			appsMap[v.ID] = v
-		}
-
-		// Add app to changes
-		for k, v := range changes {
-
-			for _, vv := range v.Change.GetAppIDs() {
-
-				if val, ok := appsMap[vv]; ok {
-
-					changes[k].Apps = append(changes[k].Apps, val)
-				}
-			}
-		}
-
-		wg.Done()
-
-	}()
-
-	// Get packages
-	wg.Add(1)
-	go func() {
-
-		// Get package IDs
-		var packageIDs []int
-		for _, v := range changes {
-			packageIDs = append(packageIDs, v.Change.GetPackageIDs()...)
-		}
-
-		// Get packages for all changes
-		packagesMap := make(map[int]db.Package)
-		packages, err := db.GetPackages(packageIDs, []string{"id", "name"})
-		logger.Error(err)
-
-		// Make package map
-		for _, v := range packages {
-			packagesMap[v.ID] = v
-		}
-
-		// Add app to changes
-		for k, v := range changes {
-
-			for _, vv := range v.Change.GetAppIDs() {
-
-				if val, ok := packagesMap[vv]; ok {
-
-					changes[k].Packages = append(changes[k].Packages, val)
-				}
-			}
-		}
-
-		wg.Done()
-
-	}()
-
-	// Wait
-	wg.Wait()
 
 	// Template
 	t := changesTemplate{}
