65abaa377b9ff1c8a492ba300e46adf65d9e5c4b
James Eagle <jimeagle@gmail.com>
2018-10-24 21:25:44 +0100

    Fix build

--- main.go
+++ main.go
@@ -70,3 +70,28 @@ func main() {
 		<-forever
 	}
 }
+
+func configSetup() {
+
+	// Google
+	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", os.Getenv("STEAM_GOOGLE_APPLICATION_CREDENTIALS"))
+	}
+
+	//
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("STEAM")
+
+	// Rabbit
+	viper.SetDefault("RABBIT_USER", "guest")
+	viper.SetDefault("RABBIT_PASS", "guest")
+
+	// Other
+	viper.SetDefault("PORT", "8081")
+	viper.SetDefault("ENV", "local")
+	viper.SetDefault("MEMCACHE_DSN", "memcache:11211")
+	viper.SetDefault("PATH", "/root")
+	viper.SetDefault("MYSQL_DSN", "root@tcp(localhost:3306)/steam?parseTime=true")
+	viper.SetDefault("DOMAIN", "https://gamedb.online")
+	viper.SetDefault("SHORT_NAME", "GameDB")
+}
--- web/app.go
+++ web/app.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gamedb/website/helpers"
 	"github.com/gamedb/website/logging"
 	"github.com/go-chi/chi"
+	"github.com/grokify/html-strip-tags-go"
 )
 
 func AppHandler(w http.ResponseWriter, r *http.Request) {
