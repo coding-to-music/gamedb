c03cf933c54c483114f12a4248ea815146b87d71
James Eagle <jimeagle@gmail.com>
2018-10-28 20:45:31 +0000

    Fix sitemap

--- web/site_map.go
+++ web/site_map.go
@@ -34,13 +34,17 @@ var pages = []string{
 
 func SiteMapHandler(w http.ResponseWriter, r *http.Request) {
 
-	sm := stm.NewSitemap()
+	sm := stm.NewSitemap(1)
 	sm.SetDefaultHost("https://gamedb.online/")
 	sm.SetCompress(true)
 	sm.Create()
 
 	for _, v := range pages {
-		sm.Add(stm.URL{"loc": v, "changefreq": "daily", "mobile": true})
+		sm.Add(stm.URL{
+			{"loc", v},
+			{"changefreq", "daily"},
+			{"mobile", true},
+		})
 	}
 
 	w.Write(sm.XMLContent())
