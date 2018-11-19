dfaf3416ca75b16a41b13b88a0256cca88051feb
James Eagle <jimeagle@gmail.com>
2018-11-19 18:00:18 +0000

    Fix recaptcha forms

--- assets/js/recaptcha.js
+++ assets/js/recaptcha.js
@@ -1,16 +1,15 @@
-function recaptchaCallback() {
+function recaptchaCallback(code) {
 
-    $('form button[type=submit]').prop("disabled", false);
+    $('form[data-recaptcha] button[type=submit]').prop("disabled", false);
 
 
-    const inputs = $('form input[type=text], form input[type=email], form textarea').filter(function () {
-        return $(this).val() === "";
+    const inputs = $('form[data-recaptcha] input[type=text], form input[type=email], form textarea').filter(function () {
+        return $(this).val() === '';
     });
 
     if (inputs.length > 0) {
         inputs.get(0).focus();
     } else {
-        $('form').submit();
+        $('form[data-recaptcha]').submit();
     }
-
 }
