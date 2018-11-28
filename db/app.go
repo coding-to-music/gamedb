62328fa11a397c5c1a03e319500799092a20d32f
James Eagle <jimeagle@gmail.com>
2018-11-28 19:56:33 +0000

    Allow some steam errors

--- db/app.go
+++ db/app.go
@@ -892,9 +892,9 @@ func (app *App) UpdateFromAPI() (errs []error) {
 
 		percentages, _, err := helpers.GetSteam().GetGlobalAchievementPercentagesForApp(app.ID)
 
-		// This endpoint returns 403 if the app has no achievement data, so it's probably fine.
+		// This endpoint seems to error if the app has no achievement data, so it's probably fine.
 		err2, ok := err.(steam.Error)
-		if ok && err2.Code() == 403 {
+		if ok && (err2.Code() == 403 || err2.Code() == 500) {
 			err = nil
 		}
 
@@ -924,6 +924,13 @@ func (app *App) UpdateFromAPI() (errs []error) {
 	go func(app *App) {
 
 		schema, _, err := helpers.GetSteam().GetSchemaForGame(app.ID)
+
+		// This endpoint seems to error if the app has no schema, so it's probably fine.
+		err2, ok := err.(steam.Error)
+		if ok && (err2.Code() == 403) {
+			err = nil
+		}
+
 		if err != nil {
 
 			errs = append(errs, err)
