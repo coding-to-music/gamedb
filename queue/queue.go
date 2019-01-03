a5e2c4fbf32317faa60ee989525ddfede204ee50
James Eagle <jimeagle@gmail.com>
2019-01-03 22:06:21 +0000

    Log consumer name

--- queue/queue.go
+++ queue/queue.go
@@ -184,7 +184,7 @@ func (s rabbitConsumer) consume() {
 
 					requeue, err := s.Message.process(msg)
 					if err != nil {
-						logError(err)
+						logError(err, s.Message.getConsumeQueue().String())
 					}
 
 					// Might be getting rate limited
