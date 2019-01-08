60323e9201f882da9e5898a0c90eecb819842e56
James Eagle <jimeagle@gmail.com>
2019-01-08 13:25:46 +0000

    Update queue log

--- queue/queue.go
+++ queue/queue.go
@@ -186,7 +186,7 @@ func (s rabbitConsumer) consume() {
 
 					requeue, err := s.Message.process(msg)
 					if err != nil {
-						logError(err, "(Queue: "+s.Message.getConsumeQueue().String()+")")
+						logError(err, s.Message.getConsumeQueue().String())
 					}
 
 					// Might be getting rate limited
