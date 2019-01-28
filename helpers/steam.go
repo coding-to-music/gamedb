d5fe69535242d8431ba65c1d63b3b8f1454f514d
James Eagle <jimeagle@gmail.com>
2019-01-28 21:01:05 +0000

    Update steam client

--- helpers/steam.go
+++ helpers/steam.go
@@ -7,15 +7,21 @@ import (
 	"github.com/gamedb/website/config"
 )
 
-var steamClient = &steam.Steam{
-	Key:        config.Config.SteamAPIKey,
-	LogChannel: GetSteamLogsChan(),
-	UserAgent:  "http://gamedb.online",
-	APIRate:    time.Millisecond * 1000,
-	StoreRate:  time.Millisecond * 1600,
-}
+var steamClient *steam.Steam
 
 func GetSteam() *steam.Steam {
+
+	if steamClient == nil {
+
+		steamClient = new(steam.Steam)
+		steamClient.SetKey(config.Config.SteamAPIKey)
+		steamClient.SetUserAgent("http://gamedb.online")
+		steamClient.SetLogChannel(GetSteamLogsChan())
+		steamClient.SetAPIRateLimit(time.Millisecond*1000, 10)
+		steamClient.SetStoreRateLimit(time.Millisecond*1600, 10)
+
+	}
+
 	return steamClient
 }
 
