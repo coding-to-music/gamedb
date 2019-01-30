15e99f2ba9ecf635dcd7314f9fbee40d480a243b
James Eagle <jimeagle@gmail.com>
2019-01-30 12:52:09 +0000

    Don't highlight games of previously logged in user

--- assets/js/_helpers.js
+++ assets/js/_helpers.js
@@ -38,16 +38,18 @@ function toast(success = true, body, title = '', timeout = 0, link = '') {
 }
 
 function highLightOwnedGames() {
-    let games = localStorage.getItem('games');
-    if (games != null) {
-        games = JSON.parse(games);
+    if (user.isLoggedIn) {
+        let games = localStorage.getItem('games');
         if (games != null) {
-            $('[data-app-id]').each(function () {
-                const id = $(this).attr('data-app-id');
-                if (games.indexOf(parseInt(id)) !== -1) {
-                    $(this).addClass('font-weight-bold')
-                }
-            });
+            games = JSON.parse(games);
+            if (games != null) {
+                $('[data-app-id]').each(function () {
+                    const id = $(this).attr('data-app-id');
+                    if (games.indexOf(parseInt(id)) !== -1) {
+                        $(this).addClass('font-weight-bold')
+                    }
+                });
+            }
         }
     }
 }
