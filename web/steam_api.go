5e469c39368a7ec776c5ac82dd9b5ab21d693071
James Eagle <jimeagle@gmail.com>
2019-03-09 17:14:42 +0000

    Show undocumented

--- web/steam_api.go
+++ web/steam_api.go
@@ -20,7 +20,34 @@ func steamAPIHandler(w http.ResponseWriter, r *http.Request) {
 
 	t := steamAPITemplate{}
 	t.Fill(w, r, "Steam API", "")
-	t.Interfaces = make(Interfaces)
+	t.Interfaces = Interfaces{}
+	t.addDocumented(w, r)
+	t.addUndocumented()
+	t.Description = template.HTML(strconv.Itoa(t.Count())) + " of the known Steam web API endpoints."
+
+	err := returnTemplate(w, r, "steam_api", t)
+	log.Err(err, r)
+}
+
+type steamAPITemplate struct {
+	GlobalTemplate
+	Interfaces Interfaces
+}
+
+func (t steamAPITemplate) Count() (count int) {
+	for _, v := range t.Interfaces {
+		for _, vv := range v {
+			for _, vvv := range vv.Versions {
+				for range vvv {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+func (t *steamAPITemplate) addDocumented(w http.ResponseWriter, r *http.Request) {
 
 	steamResp, _, err := helpers.GetSteam().GetSupportedAPIList()
 	if err != nil {
@@ -30,8 +57,27 @@ func steamAPIHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Put into a map to remove dupes from Github
 	for _, v := range steamResp.Interfaces {
-		t.Interfaces.addInterface(v)
+		t.Interfaces.addInterface(v, true)
 	}
+}
+
+type Interfaces map[string]Methods
+
+type Methods map[string]Method
+
+type Method struct {
+	Versions   map[int]map[string]map[string]Param
+	Documented bool
+	Publisher  bool
+}
+
+type Param struct {
+	Type        string `json:"type"`
+	Optional    bool   `json:"optional"`
+	Description string `json:"description"`
+}
+
+func (t *steamAPITemplate) addUndocumented() {
 
 	client, ctx := helpers.GetGithub()
 	_, dir, _, err := client.Repositories.GetContents(ctx, "SteamDatabase", "SteamTracking", "API", nil)
@@ -68,37 +114,14 @@ func steamAPIHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			t.Interfaces.addInterface(i)
+			t.Interfaces.addInterface(i, false)
 		}(v)
 	}
 
 	wg.Wait()
-
-	for _, v := range t.Interfaces {
-		for _, vv := range v {
-			for _, vvv := range vv {
-				for range vvv {
-					t.Count++
-				}
-			}
-		}
-	}
-
-	t.Description = template.HTML(strconv.Itoa(t.Count)) + " of the known Steam web API endpoints."
-
-	err = returnTemplate(w, r, "steam_api", t)
-	log.Err(err, r)
 }
 
-type steamAPITemplate struct {
-	GlobalTemplate
-	Interfaces Interfaces
-	Count      int
-}
-
-type Interfaces map[string]Interface
-
-func (i *Interfaces) addInterface(in steam.APIInterface) {
+func (i *Interfaces) addInterface(in steam.APIInterface, documented bool) {
 
 	addMutex.Lock()
 	defer addMutex.Unlock()
@@ -108,34 +131,29 @@ func (i *Interfaces) addInterface(in steam.APIInterface) {
 		for _, param := range method.Parameters {
 
 			if (*i)[in.Name] == nil {
-				(*i)[in.Name] = make(map[string]map[int]map[string]map[string]Param)
+				(*i)[in.Name] = map[string]Method{}
 			}
 
-			if (*i)[in.Name][method.Name] == nil {
-				(*i)[in.Name][method.Name] = make(map[int]map[string]map[string]Param)
+			if (*i)[in.Name][method.Name].Versions == nil {
+				(*i)[in.Name][method.Name] = Method{
+					Documented: documented,
+					Versions:   map[int]map[string]map[string]Param{},
+				}
 			}
 
-			if (*i)[in.Name][method.Name][method.Version] == nil {
-				(*i)[in.Name][method.Name][method.Version] = make(map[string]map[string]Param)
+			if (*i)[in.Name][method.Name].Versions[method.Version] == nil {
+				(*i)[in.Name][method.Name].Versions[method.Version] = make(map[string]map[string]Param)
 			}
 
-			if (*i)[in.Name][method.Name][method.Version][method.HTTPmethod] == nil {
-				(*i)[in.Name][method.Name][method.Version][method.HTTPmethod] = make(map[string]Param)
+			if (*i)[in.Name][method.Name].Versions[method.Version][method.HTTPmethod] == nil {
+				(*i)[in.Name][method.Name].Versions[method.Version][method.HTTPmethod] = make(map[string]Param)
 			}
 
-			(*i)[in.Name][method.Name][method.Version][method.HTTPmethod][param.Name] = Param{
+			(*i)[in.Name][method.Name].Versions[method.Version][method.HTTPmethod][param.Name] = Param{
 				Type:        param.Type,
 				Optional:    param.Optional,
 				Description: param.Description,
 			}
 		}
 	}
-}
-
-type Interface map[string]map[int]map[string]map[string]Param
-
-type Param struct {
-	Type        string `json:"type"`
-	Optional    bool   `json:"optional"`
-	Description string `json:"description"`
 }
