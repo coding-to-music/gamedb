9ecf80830b62435ce6fee17e588e930a03cb408d
James Eagle <jimeagle@gmail.com>
2019-03-28 21:58:53 +0000

    new releases page

---
+++ assets/js/new_releases.js
@@ -0,0 +1,79 @@
+if ($('#new-releases-page').length > 0) {
+
+    $('table.table-datatable2').DataTable($.extend(true, {}, dtDefaultOptions, {
+        "order": [[4, 'desc']],
+        "createdRow": function (row, data, dataIndex) {
+            $(row).attr('data-id', data[0]);
+            $(row).attr('data-link', data[3]);
+        },
+        "columnDefs": [
+            // Icon / Name
+            {
+                "targets": 0,
+                "render": function (data, type, row) {
+                    return '<img src="' + row[2] + '" class="rounded square" alt="' + row[1] + '"><span>' + row[1] + '</span>';
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).addClass('img');
+                    $(td).attr('data-app-id', rowData[0]);
+                },
+                "orderable": false
+            },
+            // App Type
+            {
+                "targets": 1,
+                "render": function (data, type, row) {
+                    return row[4];
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).attr('nowrap', 'nowrap');
+                },
+                "orderable": false
+            },
+            // Price
+            {
+                "targets": 2,
+                "render": function (data, type, row) {
+                    return row[5];
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).attr('nowrap', 'nowrap');
+                },
+                "orderable": false
+            },
+            // Score
+            {
+                "targets": 3,
+                "render": function (data, type, row) {
+                    return row[7] + '%';
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).attr('nowrap', 'nowrap');
+                },
+                "orderable": false
+            },
+            // Players
+            {
+                "targets": 4,
+                "render": function (data, type, row) {
+                    return row[8].toLocaleString();
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).attr('nowrap', 'nowrap');
+                },
+                "orderable": false
+            },
+            // Release Date
+            {
+                "targets": 5,
+                "render": function (data, type, row) {
+                    return row[6];
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).attr('nowrap', 'nowrap');
+                },
+                "orderable": false
+            }
+        ]
+    }));
+}
---
+++ web/new_releases.go
@@ -0,0 +1,101 @@
+package web
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gamedb/website/log"
+	"github.com/gamedb/website/session"
+	"github.com/gamedb/website/sql"
+	"github.com/go-chi/chi"
+)
+
+func newReleasesRouter() http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", newReleasesHandler)
+	r.Get("/new-releases.json", newReleasesAjaxHandler)
+	return r
+}
+
+func newReleasesHandler(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+
+	t := newReleasesTemplate{}
+	t.fill(w, r, "New Releases", "")
+
+	t.Apps, err = countUpcomingApps()
+	log.Err(err, r)
+
+	err = returnTemplate(w, r, "new_releases", t)
+	log.Err(err, r)
+}
+
+type newReleasesTemplate struct {
+	GlobalTemplate
+	Apps int
+}
+
+func newReleasesAjaxHandler(w http.ResponseWriter, r *http.Request) {
+
+	query := DataTablesQuery{}
+	err := query.fillFromURL(r.URL.Query())
+	log.Err(err, r)
+
+	var count int
+	var apps []sql.App
+
+	gorm, err := sql.GetMySQLClient()
+	if err != nil {
+		log.Err(err, r)
+		return
+	}
+
+	columns := map[string]string{
+		"0": "name",
+		"2": "price",
+		"3": "reviews_score",
+		"4": "player_peak_week",
+		"5": "release_date_unix",
+	}
+
+	var code = session.GetCountryCode(r)
+
+	gorm = gorm.Model(sql.App{})
+	gorm = gorm.Select([]string{"id", "name", "icon", "type", "prices", "release_date_unix", "player_peak_week", "reviews_score"})
+	gorm = gorm.Where("release_date_unix < ?", time.Now().Unix())
+	gorm = gorm.Where("release_date_unix > ?", time.Now().Add(time.Hour * 24 * 7 * -1).Unix())
+	gorm = gorm.Order(query.getOrderSQL(columns, code))
+
+	// Count before limitting
+	gorm.Count(&count)
+	log.Err(gorm.Error, r)
+
+	gorm = gorm.Limit(100)
+	gorm = gorm.Offset(query.getOffset())
+
+	gorm = gorm.Find(&apps)
+	log.Err(gorm.Error, r)
+
+	response := DataTablesAjaxResponse{}
+	response.RecordsTotal = strconv.Itoa(count)
+	response.RecordsFiltered = strconv.Itoa(count)
+	response.Draw = query.Draw
+
+	for _, app := range apps {
+		response.AddRow([]interface{}{
+			app.ID,                                 // 0
+			app.GetName(),                          // 1
+			app.GetIcon(),                          // 2
+			app.GetPath(),                          // 3
+			app.GetType(),                          // 4
+			sql.GetPriceFormatted(app, code).Final, // 5
+			app.GetReleaseDateNice(),               // 6
+			app.ReviewsScore,                       // 7
+			app.PlayerPeakWeek,                     // 8
+		})
+	}
+
+	response.output(w, r)
+}
