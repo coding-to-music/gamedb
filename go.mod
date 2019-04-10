d432314d270a9402e6bf66e4f9dcbe3f314ac55d
James Eagle <jimeagle@gmail.com>
2019-04-10 12:46:55 +0100

    Move Patreon code

--- go.mod
+++ go.mod
@@ -12,10 +12,10 @@ require (
 	github.com/Jleagle/google-cloud-storage-go v0.0.0-20181227195340-0633133a5c6c
 	github.com/Jleagle/influxql v0.0.0-20190326222351-022d7e9bc683
 	github.com/Jleagle/memcache-go v0.0.0-20190306211229-e1529000ed89
+	github.com/Jleagle/patreon-go v0.0.0-20190410080127-713ac5bb667d
 	github.com/Jleagle/recaptcha-go v0.0.0-20190220085232-0e548dc7cc83
 	github.com/Jleagle/sitemap-go v0.0.0-20190405195207-2bdddbb3bd50
 	github.com/Jleagle/steam-go v0.0.0-20190407204527-e1ba167dd5af
-	github.com/Jleagle/unmarshal-go v0.0.0-20190220084824-1808d9beaef9 // indirect
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/antchfx/htmlquery v1.0.0 // indirect
 	github.com/antchfx/xmlquery v1.0.0 // indirect
@@ -49,7 +49,6 @@ require (
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/jinzhu/now v1.0.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/kr/pretty v0.1.0
 	github.com/leekchan/accounting v0.0.0-20180703100437-18a1925d6514
 	github.com/lib/pq v1.0.0 // indirect
 	github.com/logrusorgru/aurora v0.0.0-20181002194514-a7b3b318ed4e
--- web/patreon.go
+++ web/patreon.go
@@ -1,14 +1,10 @@
 package web
 
 import (
-	"crypto/hmac"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"time"
 
+	"github.com/Jleagle/patreon-go/patreon"
 	"github.com/gamedb/website/config"
 	"github.com/gamedb/website/log"
 	"github.com/gamedb/website/mongo"
@@ -23,144 +19,64 @@ func patreonRouter() http.Handler {
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
 
-	event := r.Header.Get("X-Patreon-Event")
-	signature := r.Header.Get("X-Patreon-Signature")
-
-	if event == "" || signature == "" {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte("missing event or signature header"))
-		log.Err(err)
-		return
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
+	b, event, err := patreon.ValidateRequest(r, config.Config.PatreonSecret)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte(err.Error()))
 		log.Err(err)
 		return
 	}
 
-	hash := hmac.New(md5.New, []byte(config.Config.PatreonSecret))
-	if _, err := hash.Write(b); err != nil {
+	pwr, err := patreon.UnmarshalBytes(b)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte(err.Error()))
 		log.Err(err)
 		return
 	}
 
-	sum := hash.Sum(nil)
-	expectedSignature := hex.EncodeToString(sum)
-
-	if expectedSignature != signature {
+	err = saveWebhookToMongo(event, pwr, b)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte("invalid webhook signature"))
 		log.Err(err)
 		return
 	}
 
-	pwr, err := unmarshalWebhook(b)
+	err = saveWebhookEvent(r, event, pwr)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Err(err)
 		return
 	}
-
-	err = saveWebhookToMongo(event, b)
-	if err != nil {
-		log.Err(err)
-		return
-	}
-
-	err = saveWebhookEvent(r, pwr)
-	if err != nil {
-		log.Err(err)
-		return
-	}
-
-	log.Err(err)
 }
 
-func unmarshalWebhook(b []byte) (pwr mongo.PatreonWebhookRaw, err error) {
-
-	err = json.Unmarshal(b, &pwr)
-	if err != nil {
-		return pwr, err
-	}
-
-	for _, v := range pwr.Included {
-
-		typ := mongo.PatreonWebhookRawType{}
-		err = json.Unmarshal(v, &typ)
-		if err != nil {
-			return pwr, err
-		}
-
-		switch typ.Type {
-		case "campaign":
-
-			include := mongo.PatreonWebhookRawIncludedCampaign{}
-			err = json.Unmarshal(v, &include)
-			if err != nil {
-				return pwr, err
-			}
-			pwr.Campaign = include
-
-		case "user":
-
-			include := mongo.PatreonWebhookRawIncludedUser{}
-			err = json.Unmarshal(v, &include)
-			if err != nil {
-				return pwr, err
-			}
-			pwr.User = include
-
-		case "reward":
-
-			include := mongo.PatreonWebhookRawIncludedReward{}
-			err = json.Unmarshal(v, &include)
-			if err != nil {
-				return pwr, err
-			}
-			pwr.Rewards = append(pwr.Rewards, include)
-
-		case "goal":
-
-			include := mongo.PatreonWebhookRawIncludedGoals{}
-			err = json.Unmarshal(v, &include)
-			if err != nil {
-				return pwr, err
-			}
-			pwr.Goals = append(pwr.Goals, include)
-
-		default:
-			log.Warning("Missing webhook data")
-		}
-	}
-
-	return pwr, nil
-}
-
-func saveWebhookToMongo(event string, pwr mongo.PatreonWebhookRaw, body []byte) (err error) {
+func saveWebhookToMongo(event string, pwr patreon.Webhook, body []byte) (err error) {
 
 	_, err = mongo.InsertDocument(mongo.CollectionPatreonWebhooks, mongo.PatreonWebhook{
-		CreatedAt:   time.Now(),
-		RequestBody: string(body),
-		Event:       event,
-		Email:       pwr.User.Attributes.Email,
+		CreatedAt:               time.Now(),
+		RequestBody:             string(body),
+		Event:                   event,
+		Email:                   pwr.User.Attributes.Email,
+		PatronStatus:            pwr.Data.Attributes.PatronStatus,
+		LifetimeSupportCents:    pwr.Data.Attributes.LifetimeSupportCents,
+		PledgeAmountCents:       pwr.Data.Attributes.PledgeAmountCents,
+		PledgeCapAmountCents:    int(pwr.Data.Attributes.PledgeCapAmountCents),
+		PledgeRelationshipStart: pwr.Data.Attributes.PledgeRelationshipStart,
 	})
 	return err
 }
 
-func saveWebhookEvent(r *http.Request, pwr mongo.PatreonWebhookRaw) (err error) {
+func saveWebhookEvent(r *http.Request, event string, pwr patreon.Webhook) (err error) {
 
 	if pwr.User.Attributes.Email != "" {
 		player := mongo.Player{}
 		err = mongo.FindDocument(mongo.CollectionPlayers, "email", pwr.User.Attributes.Email, mongo.M{"_id": 1}, &player)
+		if err == mongo.ErrNoDocuments {
+			return nil
+		}
 		if err != nil {
 			return err
 		}
 
-		return mongo.CreateEvent(r, player.ID, mongo.EventRefresh)
+		return mongo.CreateEvent(r, player.ID, mongo.EventPatreon+"-"+event)
 	}
 
 	return nil
