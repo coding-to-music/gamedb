15bfc9d7c16aa4d8d9de08b9ca356b932b40395b
James Eagle <jimeagle@gmail.com>
2019-04-17 12:40:51 +0100

    Put consumer key into logs

--- log/log.go
+++ log/log.go
@@ -181,6 +181,7 @@ func log(interfaces ...interface{}) {
 			Payload:   entry.toText(true),
 			Labels: map[string]string{
 				"env": config.Config.Environment.Get(),
+				"key": config.Config.SteamAPIKey[0:5],
 			},
 		})
 	}
