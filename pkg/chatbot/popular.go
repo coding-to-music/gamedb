ba04968970169ac4c392d51c0d621d044992b5ac
James Eagle <jimeagle@gmail.com>
2019-04-18 21:48:41 +0100

    Make popular embedded

--- pkg/chatbot/popular.go
+++ pkg/chatbot/popular.go
@@ -18,18 +18,8 @@ func (c CommandPopular) Regex() *regexp.Regexp {
 func (c CommandPopular) Output(input string) (message discordgo.MessageSend, err error) {
 
 	message.Embed = &discordgo.MessageEmbed{
-		Title: "Popular Apps",
-		URL:   "https://gamedb.online/apps/440",
-		// Fields: [],
-		Author: &discordgo.MessageEmbedAuthor{
-			Name:    "gamedb.online",
-			URL:     "https://gamedb.online/",
-			IconURL: "https://gamedb.online/assets/img/sa-bg-32x32.png",
-		},
-		Footer: &discordgo.MessageEmbedFooter{
-			Text:    "x",
-			IconURL: "https://gamedb.online/assets/img/sa-bg-32x32.png",
-		},
+		Title:  "Popular Apps",
+		Author: author,
 	}
 
 	apps, err := sql.PopularApps()
@@ -41,7 +31,7 @@ func (c CommandPopular) Output(input string) (message discordgo.MessageSend, err
 		for k, v := range apps[0:10] {
 
 			message.Embed.Fields = append(message.Embed.Fields, &discordgo.MessageEmbedField{
-				Name:  helpers.OrdinalComma(k),
+				Name:  helpers.OrdinalComma(k + 1),
 				Value: v.GetName(), // both fields are needed
 			})
 		}
