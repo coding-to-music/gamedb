e375f9e4aa8e41a7f81ba19a6c1274eeb769f7e6
James Eagle <jimeagle@gmail.com>
2019-04-19 18:29:57 +0100

    Lazy load events

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -28,70 +28,88 @@ if ($('#settings-page').length > 0) {
     //     }
     // });
 
-    // Data tables
-    $('#events table.table-datatable2').DataTable($.extend(true, {}, dtDefaultOptions, {
-        "ajax": function (data, callback, settings) {
+    // On tab change
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 
-            delete data.columns;
-            delete data.length;
-            delete data.search;
+        const to = $(e.target);
+        const from = $(e.relatedTarget);
 
-            $.ajax({
-                url: $(this).attr('data-path'),
-                data: data,
-                success: callback,
-                dataType: 'json',
-                cache: $(this).attr('data-cache') !== "false"
-            });
-        },
-        "order": [[0, 'desc']],
-        "columnDefs": [
-            // Time
-            {
-                "targets": 0,
-                "render": function (data, type, row) {
-                    return '<span data-toggle="tooltip" data-placement="left" title="' + row[1] + '" data-livestamp="' + row[0] + '">' + row[1] + '</span>';
-                },
-                "createdCell": function (td, cellData, rowData, row, col) {
-                    $(td).attr('nowrap', 'nowrap');
-                },
-                "orderable": false
-            },
-            // Type
-            {
-                "targets": 1,
-                "render": function (data, type, row) {
-                    return '<i class="fas ' + row[7] + '"></i> ' + row[2];
-                },
-                "createdCell": function (td, cellData, rowData, row, col) {
-                    $(td).attr('nowrap', 'nowrap');
-                },
-                "orderable": false
-            },
-            // IP
-            {
-                "targets": 2,
-                "render": function (data, type, row) {
+        // On entering tab
+        if (to.attr('href') === '#events') {
+            if (!to.attr('loaded')) {
+                to.attr('loaded', 1);
 
-                    if (row[3] === row[6]) {
-                        return '<span class="font-weight-bold" data-toggle="tooltip" data-placement="left" title="Your current IP">' + row[3] + '</span>';
-                    }
-                    return row[3];
-                },
-                "orderable": false
-            },
-            // User Agent
-            {
-                "targets": 3,
-                "render": function (data, type, row) {
-                    // return row[4];
-                    return '<span data-toggle="tooltip" data-placement="left" title="' + row[4] + '">' + row[5] + '</span>';
-                },
-                "createdCell": function (td, cellData, rowData, row, col) {
-                    $(td).attr('nowrap', 'nowrap');
-                },
-                "orderable": false
+                loadEvents();
             }
-        ]
-    }));
+        }
+    });
+
+    function loadEvents() {
+
+        $('#events table.table-datatable2').DataTable($.extend(true, {}, dtDefaultOptions, {
+            "ajax": function (data, callback, settings) {
+
+                delete data.columns;
+                delete data.length;
+                delete data.search;
+
+                $.ajax({
+                    url: $(this).attr('data-path'),
+                    data: data,
+                    success: callback,
+                    dataType: 'json',
+                    cache: $(this).attr('data-cache') !== "false"
+                });
+            },
+            "order": [[0, 'desc']],
+            "columnDefs": [
+                // Time
+                {
+                    "targets": 0,
+                    "render": function (data, type, row) {
+                        return '<span data-toggle="tooltip" data-placement="left" title="' + row[1] + '" data-livestamp="' + row[0] + '">' + row[1] + '</span>';
+                    },
+                    "createdCell": function (td, cellData, rowData, row, col) {
+                        $(td).attr('nowrap', 'nowrap');
+                    },
+                    "orderable": false
+                },
+                // Type
+                {
+                    "targets": 1,
+                    "render": function (data, type, row) {
+                        return '<i class="fas ' + row[7] + '"></i> ' + row[2];
+                    },
+                    "createdCell": function (td, cellData, rowData, row, col) {
+                        $(td).attr('nowrap', 'nowrap');
+                    },
+                    "orderable": false
+                },
+                // IP
+                {
+                    "targets": 2,
+                    "render": function (data, type, row) {
+
+                        if (row[3] === row[6]) {
+                            return '<span class="font-weight-bold" data-toggle="tooltip" data-placement="left" title="Your current IP">' + row[3] + '</span>';
+                        }
+                        return row[3];
+                    },
+                    "orderable": false
+                },
+                // User Agent
+                {
+                    "targets": 3,
+                    "render": function (data, type, row) {
+                        // return row[4];
+                        return '<span data-toggle="tooltip" data-placement="left" title="' + row[4] + '">' + row[5] + '</span>';
+                    },
+                    "createdCell": function (td, cellData, rowData, row, col) {
+                        $(td).attr('nowrap', 'nowrap');
+                    },
+                    "orderable": false
+                }
+            ]
+        }));
+    }
 }
