1afd58e9891c9c3830ccf99c050e51f2b81ac507
James Eagle <jimeagle@gmail.com>
2019-04-27 17:51:54 +0100

    Only save cookies if need to

--- cmd/webserver/pages/webserver.go
+++ cmd/webserver/pages/webserver.go
@@ -490,7 +490,7 @@ func (t *GlobalTemplate) setFlashes(w http.ResponseWriter, r *http.Request, save
 	t.flashesBad, err = session.GetBadFlashes(w, r)
 	log.Err(err, r)
 
-	if save {
+	if save && (len(t.flashesBad) > 0 || len(t.flashesBad) > 0) {
 		err = session.Save(w, r)
 		log.Err(err)
 	}
