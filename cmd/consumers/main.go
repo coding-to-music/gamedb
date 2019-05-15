23a707b31a855129d298f7dac7b4fdd07bbd0173
James Eagle <jimeagle@gmail.com>
2019-05-15 18:09:03 +0100

    Pprof consumers

--- cmd/consumers/main.go
+++ cmd/consumers/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/gamedb/gamedb/pkg/config"
 	"github.com/gamedb/gamedb/pkg/helpers"
 	"github.com/gamedb/gamedb/pkg/log"
 	"github.com/gamedb/gamedb/pkg/queue"
 )
 
 func main() {
+
+	if config.IsLocal() {
+		log.Info("Starting consumers profiling")
+		go func() {
+			log.Err(http.ListenAndServe("localhost:9082", nil))
+		}()
+	}
 
 	log.Info("Starting consumers")
 
