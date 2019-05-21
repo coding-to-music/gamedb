f6ffeecfe2cb0c6a7e5b0365da268698c591ea2d
James Eagle <jimeagle@gmail.com>
2019-05-21 20:47:56 +0100

    Refactor crons

--- cmd/crons/main.go
+++ cmd/crons/main.go
@@ -16,37 +16,37 @@ func main() {
 	c := cron.New()
 
 	// Every 10 minutes
-	err = c.AddFunc("0 */10 * * * *", crons.SteamClientPlayers)
+	err = c.AddFunc("0 */10 * * * *", crons.CronRegister[crons.CronSteamClientPlayers])
 	log.Critical(err)
 
 	// Every 3 hours
-	err = c.AddFunc("0 0 */3 * * *", crons.AppPlayers)
+	err = c.AddFunc("0 0 */3 * * *", crons.CronRegister[crons.CronAppPlayers])
 	log.Critical(err)
 
 	// Every 6 hours
-	err = c.AddFunc("0 0 */6 * * *", crons.AutoPlayerRefreshes)
+	err = c.AddFunc("0 0 */6 * * *", crons.CronRegister[crons.CronAutoPlayerRefreshes])
 	log.Critical(err)
 
 	// Every 24 hours
-	err = c.AddFunc("0 1 0 * * *", crons.ClearUpcomingCache)
+	err = c.AddFunc("0 1 0 * * *", crons.CronRegister[crons.CronClearUpcomingCache])
 	log.Critical(err)
 
-	err = c.AddFunc("0 0 0 * * *", crons.PlayerRanks)
+	err = c.AddFunc("0 0 0 * * *", crons.CronRegister[crons.CronPlayerRanks])
 	log.Critical(err)
 
-	err = c.AddFunc("0 0 1 * * *", crons.Genres)
+	err = c.AddFunc("0 0 1 * * *", crons.CronRegister[crons.CronGenres])
 	log.Critical(err)
 
-	err = c.AddFunc("0 0 2 * * *", crons.Tags)
+	err = c.AddFunc("0 0 2 * * *", crons.CronRegister[crons.CronTags])
 	log.Critical(err)
 
-	err = c.AddFunc("0 0 3 * * *", crons.Publishers)
+	err = c.AddFunc("0 0 3 * * *", crons.CronRegister[crons.CronPublishers])
 	log.Critical(err)
 
-	err = c.AddFunc("0 0 4 * * *", crons.Developers)
+	err = c.AddFunc("0 0 4 * * *", crons.CronRegister[crons.CronDevelopers])
 	log.Critical(err)
 
-	err = c.AddFunc("0 0 12 * * *", crons.Instagram)
+	err = c.AddFunc("0 0 12 * * *", crons.CronRegister[crons.CronInstagram])
 	log.Critical(err)
 
 	c.Start()
--- pkg/crons/crons.go
+++ pkg/crons/crons.go
@@ -6,6 +6,40 @@ import (
 	"github.com/gamedb/gamedb/pkg/log"
 )
 
+type CronEnum string
+
+var (
+	CronAppPlayers          CronEnum = "update-app-players"
+	CronClearUpcomingCache  CronEnum = "clear-upcoming-apps-cache"
+	CronInstagram           CronEnum = "post-to-instagram"
+	CronPlayerRanks         CronEnum = "update-player-ranks"
+	CronAutoPlayerRefreshes CronEnum = "update-auto-player-refreshes"
+	CronSteamClientPlayers  CronEnum = "update-steam-client-players"
+	CronGenres              CronEnum = "update-stats-genres"
+	CronPublishers          CronEnum = "update-stats-publishers"
+	CronDevelopers          CronEnum = "update-stats-developers"
+	CronTags                CronEnum = "update-stats-tags"
+
+	CronRegister = map[CronEnum]func(){
+		CronAppPlayers:          AppPlayers,
+		CronClearUpcomingCache:  ClearUpcomingCache,
+		CronInstagram:           Instagram,
+		CronPlayerRanks:         PlayerRanks,
+		CronAutoPlayerRefreshes: AutoPlayerRefreshes,
+		CronGenres:              Genres,
+		CronPublishers:          Publishers,
+		CronDevelopers:          Developers,
+		CronTags:                Tags,
+		CronSteamClientPlayers:  SteamClientPlayers,
+	}
+)
+
+// type CronInterface interface {
+// 	ID() CronEnum
+// 	Name() string
+// 	Work()
+// }
+
 // Logging
 func cronLogErr(interfaces ...interface{}) {
 	log.Err(append(interfaces, log.LogNameCron, log.LogNameGameDB)...)
@@ -16,6 +50,7 @@ func cronLogInfo(interfaces ...interface{}) {
 }
 
 func statsLogger(tableName string, count int, total int, rowName string) {
-
 	cronLogInfo("Updating " + tableName + " - " + strconv.Itoa(count) + " / " + strconv.Itoa(total) + ": " + rowName)
 }
+
+//
