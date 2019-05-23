c714e323bf5545814ec6bf558b6e31e144b9fbb8
James Eagle <jimeagle@gmail.com>
2019-05-23 21:52:59 +0100

    Slow group queue

--- pkg/queue/group.go
+++ pkg/queue/group.go
@@ -74,7 +74,7 @@ func (q groupQueue) processMessages(msgs []amqp.Delivery) {
 		return
 	}
 
-	time.Sleep(time.Second * 30)
+	time.Sleep(time.Second * 60)
 
 	err = updateGroup(message, &group)
 	if err != nil {
