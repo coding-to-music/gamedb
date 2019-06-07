bc559e322d54bded87d9551438821906ab19c410
James Eagle <jimeagle@gmail.com>
2019-06-07 18:37:50 +0100

    Remove caching code

--- cmd/webserver/pages/badges.go
+++ cmd/webserver/pages/badges.go
@@ -21,20 +21,12 @@ func badgesHandler(w http.ResponseWriter, r *http.Request) {
 	t := badgesTemplate{}
 	t.fill(w, r, "Badges", "")
 
-	// var err error
-	// t.PlayersMap, err = getBadgeCounts()
-	// if err != nil {
-	// 	returnErrorTemplate(w, r, errorTemplate{Code: 500})
-	// 	return
-	// }
-
 	err := returnTemplate(w, r, "badges", t)
 	log.Err(err, r)
 }
 
 type badgesTemplate struct {
 	GlobalTemplate
-	// PlayersMap map[int]badgeRowTemplate
 }
 
 func (bt badgesTemplate) GetSpecialBadges() (badges []mongo.PlayerBadge) {
@@ -66,35 +58,3 @@ func (bt badgesTemplate) GetEventBadges() (badges []mongo.PlayerBadge) {
 
 	return badges
 }
-
-// type badgeRowTemplate struct {
-// 	Players int
-// 	Max     int
-// 	MaxFoil int
-// }
-
-// func getBadgeCounts() (counts map[int]badgeRowTemplate, err error) {
-//
-// 	var item = helpers.MemcacheTrendingAppsCount
-//
-// 	err = helpers.GetMemcache().GetSetInterface(item.Key, item.Expiration, &counts, func() (interface{}, error) {
-//
-// 		var counts = map[int]badgeRowTemplate{}
-//
-// 		for _, v := range mongo.Badges {
-// 			if v.IsSpecial() {
-// 				counts[v.BadgeID] = badgeRowTemplate{
-//
-// 				}
-// 			} else {
-// 				counts[v.AppID] = badgeRowTemplate{
-//
-// 				}
-// 			}
-// 		}
-//
-// 		return counts, nil
-// 	})
-//
-// 	return counts, err
-// }
