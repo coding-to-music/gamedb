61dd6f9c9cd7d288d045a7882847d8548a4aa4ae
James Eagle <jimeagle@gmail.com>
2019-07-12 19:57:50 +0100

    Split crons

--- pkg/crons/players_auto_refresh.go
+++ pkg/crons/players_auto_refresh.go
@@ -4,53 +4,9 @@ import (
 	"strconv"
 
 	"github.com/gamedb/gamedb/pkg/log"
-	"github.com/gamedb/gamedb/pkg/mongo"
 	"github.com/gamedb/gamedb/pkg/queue"
 	"github.com/gamedb/gamedb/pkg/sql"
 )
-
-type PlayerRanks struct {
-}
-
-func (c PlayerRanks) ID() CronEnum {
-	return CronPlayerRanks
-}
-
-func (c PlayerRanks) Name() string {
-	return "Update player ranks"
-}
-
-func (c PlayerRanks) Config() sql.ConfigType {
-	return sql.ConfRanksUpdated
-}
-
-func (c PlayerRanks) Work() {
-
-	started(c)
-
-	cronLogInfo("Level")
-	err := mongo.RankPlayers("level", "level_rank")
-	log.Warning(err)
-
-	cronLogInfo("Games")
-	err = mongo.RankPlayers("games_count", "games_rank")
-	log.Warning(err)
-
-	cronLogInfo("Badges")
-	err = mongo.RankPlayers("badges_count", "badges_rank")
-	log.Warning(err)
-
-	cronLogInfo("Time")
-	err = mongo.RankPlayers("play_time", "play_time_rank")
-	log.Warning(err)
-
-	cronLogInfo("Friends")
-	err = mongo.RankPlayers("friends_count", "friends_rank")
-	log.Warning(err)
-
-	//
-	finished(c)
-}
 
 type AutoPlayerRefreshes struct {
 }
