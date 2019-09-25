bed87352e6460f0f3f4d019d9f06737d4c098739
James Eagle <jimeagle@gmail.com>
2019-09-25 21:30:05 +0100

    Add alt

--- cmd/webserver/assets/js/upcoming.js
+++ cmd/webserver/assets/js/upcoming.js
@@ -33,7 +33,7 @@ if ($('#upcoming-page').length > 0) {
             {
                 "targets": 0,
                 "render": function (data, type, row) {
-                    return '<div class="icon-name"><div class="icon"><img data-lazy="' + row[2] + '" data-lazy-alt="' + row[1] + '"></div><div class="name">' + row[1] + '</div></div>'
+                    return '<div class="icon-name"><div class="icon"><img data-lazy="' + row[2] + '" alt="" data-lazy-alt="' + row[1] + '"></div><div class="name">' + row[1] + '</div></div>'
                 },
                 "createdCell": function (td, cellData, rowData, row, col) {
                     $(td).addClass('img');
