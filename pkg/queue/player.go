dfb7f9e2bba0fb450b628539b2c99242e35aa49f
James Eagle <jimeagle@gmail.com>
2019-10-14 12:47:38 +0100

    Improve log

--- pkg/queue/player.go
+++ pkg/queue/player.go
@@ -861,7 +861,7 @@ func updatePlayerComments(player *mongo.Player) error {
 
 	resp, b, err := helpers.GetSteam().GetComments(player.ID, 1000000, 0)
 	if err != nil {
-		log.Err(err, b)
+		log.Err(player.ID, err, b)
 		return err
 	}
 
