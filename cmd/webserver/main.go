eb38928ab05c896cd28b4dcb48973589f8588076
James Eagle <jimeagle@gmail.com>
2019-10-31 20:46:33 +0000

    Increase cache

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -163,7 +163,7 @@ func fileServer(r chi.Router, path string, root http.FileSystem) {
 	}
 
 	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		pages.SetCacheHeaders(w, time.Hour*24*28)
+		pages.SetCacheHeaders(w, time.Hour*24*365)
 		fs.ServeHTTP(w, r)
 	}))
 }
