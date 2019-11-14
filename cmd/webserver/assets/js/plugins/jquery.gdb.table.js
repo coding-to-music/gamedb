d9b1e19f8e0aa5d099b86702fda046358874a5d7
James Eagle <jimeagle@gmail.com>
2019-11-14 21:28:13 +0000

    Stop saving page/sort/order

--- cmd/webserver/assets/js/plugins/jquery.gdb.table.js
+++ cmd/webserver/assets/js/plugins/jquery.gdb.table.js
@@ -200,23 +200,23 @@
                     : $pagination.show();
 
                 // Update URL
-                if ($(parent.element).is(":visible")) {
-
-                    const order = dt.order();
-                    if (JSON.stringify(parent.initialValues.s) === JSON.stringify(order)) {
-                        deleteUrlParam('o');
-                        deleteUrlParam('s');
-                    } else {
-                        setUrlParam('o', order[0][1]);
-                        setUrlParam('s', order[0][0]);
-                    }
-
-                    if (dt.page.info().page === 0) {
-                        deleteUrlParam('p');
-                    } else {
-                        setUrlParam('p', dt.page.info().page + 1);
-                    }
-                }
+                // if ($(parent.element).is(":visible")) {
+                //
+                //     const order = dt.order();
+                //     if (JSON.stringify(parent.initialValues.s) === JSON.stringify(order)) {
+                //         deleteUrlParam('o');
+                //         deleteUrlParam('s');
+                //     } else {
+                //         setUrlParam('o', order[0][1]);
+                //         setUrlParam('s', order[0][0]);
+                //     }
+                //
+                //     if (dt.page.info().page === 0) {
+                //         deleteUrlParam('p');
+                //     } else {
+                //         setUrlParam('p', dt.page.info().page + 1);
+                //     }
+                // }
 
                 // Bold rows
                 parent.highlightRows();
