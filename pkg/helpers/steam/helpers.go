7c4a6cce26e36d25875d92e7de55596baf6b980f
James Eagle <jimeagle@gmail.com>
2019-11-22 18:07:49 +0000

    Sleep on all api errors

--- pkg/helpers/steam/helpers.go
+++ pkg/helpers/steam/helpers.go
@@ -36,15 +36,14 @@ func LogSteamError(err error, interfaces ...interface{}) {
 		// Sleeps on rate limits etc
 		if val, ok := err.(steam.Error); ok {
 
-			// Rate limit
 			if val.Code == 429 {
-				time.Sleep(time.Second * 10)
-			}
-
-			// access is denied
-			if val.Code == 403 {
+				// Rate limit
+				time.Sleep(time.Second * 30)
+			} else {
 				time.Sleep(time.Second * 5)
 			}
+
+			return false
 		}
 
 		steamErrors := []string{
