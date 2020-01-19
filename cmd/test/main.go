8fe32852a2427779983ba0b23ba306f1b6e793dc
James Eagle <jimeagle@gmail.com>
2020-01-19 23:04:11 +0000

    Sync one app

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gamedb/gamedb/pkg/config"
-	"github.com/gamedb/gamedb/pkg/helpers"
 	"github.com/gamedb/gamedb/pkg/log"
 	"github.com/gamedb/gamedb/pkg/sql"
 )
@@ -12,12 +11,9 @@ func main() {
 	config.SetVersion("test")
 	log.Initialise([]log.LogName{log.LogNameTest}, "test")
 
-	// Get API key
-	err := sql.GetAPIKey("test")
-	if err != nil {
-		log.Critical(err)
-		return
-	}
+	sApp, err := sql.GetApp(787860, nil)
+	log.Err(err)
 
-	helpers.KeepAlive()
+	err = sApp.SaveToMongo()
+	log.Err(err)
 }
