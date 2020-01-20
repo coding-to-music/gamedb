9d87fbcabaa4860e68b9f1909f59e791f92c8494
James Eagle <jimeagle@gmail.com>
2020-01-20 22:09:18 +0000

    Revert test

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gamedb/gamedb/pkg/config"
+	"github.com/gamedb/gamedb/pkg/helpers"
 	"github.com/gamedb/gamedb/pkg/log"
 	"github.com/gamedb/gamedb/pkg/sql"
 )
@@ -11,9 +12,12 @@ func main() {
 	config.SetVersion("test")
 	log.Initialise([]log.LogName{log.LogNameTest}, "test")
 
-	sApp, err := sql.GetApp(787860, nil)
-	log.Err(err)
+	// Get API key
+	err := sql.GetAPIKey("test")
+	if err != nil {
+		log.Critical(err)
+		return
+	}
 
-	err = sApp.SaveToMongo()
-	log.Err(err)
+	helpers.KeepAlive()
 }
