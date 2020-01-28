2292f334446c65a900260af83ba0c4b1ef471470
James Eagle <jimeagle@gmail.com>
2020-01-28 21:32:15 +0000

    dt column struct

---
+++ cmd/webserver/helpers/datatable/columns.go
@@ -0,0 +1,14 @@
+package datatable
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Columns map[string]Column
+
+type Column struct {
+	sortAsc     bool
+	sortDesc    bool
+	sortDefault bson.D
+	sortAppend  bson.D
+}
