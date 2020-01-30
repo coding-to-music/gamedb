83992808a4aba99ce8568ee6232ea5846d40c52f
James Eagle <jimeagle@gmail.com>
2020-01-30 22:46:54 +0000

    Sync subs

--- pkg/tasks/dev_code_run.go
+++ pkg/tasks/dev_code_run.go
@@ -1,5 +1,10 @@
 package tasks
 
+import (
+	"github.com/gamedb/gamedb/pkg/log"
+	"github.com/gamedb/gamedb/pkg/sql"
+)
+
 type DevCodeRun struct {
 	BaseTask
 }
@@ -17,6 +22,39 @@ func (c DevCodeRun) Cron() string {
 }
 
 func (c DevCodeRun) work() (err error) {
+
+	db, err := sql.GetMySQLClient()
+	if err != nil {
+		log.Err(err)
+		return
+	}
+
+	db = db.Limit(1000)
+
+	var offset int
+	for {
+
+		var packages []sql.Package
+
+		db = db.Offset(offset).Find(&packages)
+		if db.Error != nil {
+			log.Err(db.Error)
+			return
+		}
+
+		log.Info(offset)
+
+		for _, v := range packages {
+			err = v.SaveToMongo()
+			log.Err(err)
+		}
+
+		if len(packages) < 1000 {
+			break
+		}
+
+		offset += 1000
+	}
 
 	return nil
 }
