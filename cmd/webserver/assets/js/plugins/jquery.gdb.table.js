e4ca8e4e0299a685da074f1321e3e457f098b8aa
James Eagle <jimeagle@gmail.com>
2020-02-04 20:22:31 +0000

    Always show pagination

--- cmd/webserver/assets/js/plugins/jquery.gdb.table.js
+++ cmd/webserver/assets/js/plugins/jquery.gdb.table.js
@@ -206,10 +206,10 @@
                 }
 
                 // Hide empty pagination
-                const $pagination = $(parent.element).parent().find('.dt-pagination');
-                (dt.page.info().pages <= 1)
-                    ? $pagination.hide()
-                    : $pagination.show();
+                // const $pagination = $(parent.element).parent().find('.dt-pagination');
+                // (dt.page.info().pages <= 1)
+                //     ? $pagination.hide()
+                //     : $pagination.show();
 
                 // Update URL
                 if ($(parent.element).is(":visible")) {
