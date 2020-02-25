8a840532a7af228e1f3fae6836de22d8abf7eb5e
James Eagle <jimeagle@gmail.com>
2020-02-25 21:02:39 +0000

    Typo

--- cmd/webserver/pages/api/generated/generated.go
+++ cmd/webserver/pages/api/generated/generated.go
@@ -32,8 +32,8 @@ type ErrorSchema struct {
 type PaginationSchema struct {
 	Limit        int64 `json:"limit"`
 	Offset       int64 `json:"offset"`
+	PagesCurrent *int  `json:"pagesCurrent,omitempty"`
 	PagesTotal   int   `json:"pagesTotal"`
-	Pagescurrent int   `json:"pagescurrent"`
 	RowsFiltered int64 `json:"rowsFiltered"`
 	RowsTotal    int64 `json:"rowsTotal"`
 }
@@ -106,11 +106,11 @@ func GetAppsCtx(next http.Handler) http.Handler {
 
 		var err error
 
-		ctx = context.WithValue(ctx, "key-query.Scopes", []string{""})
-
 		ctx = context.WithValue(ctx, "key-cookie.Scopes", []string{""})
 
 		ctx = context.WithValue(ctx, "key-header.Scopes", []string{""})
+
+		ctx = context.WithValue(ctx, "key-query.Scopes", []string{""})
 
 		// Parameter object where we will unmarshal all parameters from the context
 		var params GetAppsParams
@@ -247,22 +247,22 @@ func HandlerFromMux(si ServerInterface, r *chi.Mux) http.Handler {
 // Base64 encoded, gzipped, json marshaled Swagger object
 var swaggerSpec = []string{
 
-	"H4sIAAAAAAAC/8xXTW/jNhD9K8K0RyVyskWx0KkpihZpF2jQtCfDh6k0lrkrfixJZWME/u/FkPq0ZcdB",
-	"jcVeYtMczpt5fDOcvEChpdGKlHeQv4AlZ7RyFBZoDH8UWnlSvv2lFgV6oVX20WnFv7liQxL52/eW1pDD",
-	"d9ngM4u7LkNjrlrL3W6XQkmusMKwJ8jhTiUMtksZwb0JdOrog3A+0Wv25tLki/CbxGAlVDgNKRirDVkv",
-	"+gTDp/Ak3VsySMFvDUEOaC1ueT1CecXPYDkQkoKlz42wVEK+hEnEIcbVDGfjVDkCslbbi91X8Hb6xiLg",
-	"JPerTj8Xi2OOrYNgHrCiRKi1tjLSxkHVuKXL8RHdnSIkaRF77LfpeKrM/yWnSQBnaXsvu315n9JnB3VK",
-	"op0NW7SQLSFXxwgQJf9twxDKUxWpVShptOO8Fao6iFCU0Jquuso4ilTokuaxJDmH1RlwwcVgv5rWxDHg",
-	"WkgRhBF1G7F//AHSmVD0eu3oXGODFbm/tcd6Pq+wXzTWtsI8tLD6i/tV1J5CgmeB8pEe81X7PQLb9NKW",
-	"k7G3vWAm2e2lsuqVfzFZsWCboijIuaM+z5bJoA92S0Vjhd8+stfo6BNtrwqtP4ngS3AFtcsuQDYZ2EQj",
-	"/qDw+vDJDWEZG1442S7POfm5IbvtD8bV6XPcIbBykC9fOrv2GerXfc3zrXBv7rohFkF0JFHUkMNHIQmr",
-	"mn6q+IfrQssB+/c6bEEKjWXbjffG5VlWoaTy32utaqEo65xyUMLXfPDRE8rkl5+Tu4d7SOGJrIsd6SYU",
-	"kyGFRkAO764X14sgI78JV5B1I0EVq43vOVTxfQk5/Eb+jvf5gEVJPvTY5TxxomTDQTTjmgiyS0Hi831s",
-	"zzeLxWHTnfcbiH+D4zm/0zb9Z1d9c3B9aY5HrjU2tYd8kYIUSshGhu+HRX44nsXyngPqSn8Gh5OQ+ByB",
-	"AlU97M0M7CqdzrG3i8VXmmOnM1loHlIiF1fcwqietnb62S6NusteRLl7TXz35RH5sYbH6oNx8/G2oWOa",
-	"eXcL3yyh3T8GEyr/Im8FPVHYmmWzbT49oUa7GUYftPMP0fLyrIZnT+LzB1KV30B++5V53X+2Zsh9bILF",
-	"Hrv/mBI9JdjNswPB01Gve8ACV+Ona7maPkjdun1mlivO25F96ogemnueZbUusN5o5/P3i/c3Gffp3Wr3",
-	"XwAAAP//eN3svqgOAAA=",
+	"H4sIAAAAAAAC/8xXTW/jNhD9K8K0RyVyskWx0KlpixZpF2jQtCfDB1Yay9wVP5aksjEC//di+KEPW3Yc",
+	"1Fj0Epvm8L3h45sh8wKVElpJlM5C+QIGrVbSoh8wremjUtKhdPGXllfMcSWLj1ZJ+s1WGxSMvn1rcA0l",
+	"fFMMmEWYtQXT+ipG7na7HGq0leGakKCEO5kR2S4nBvsm0inQB25dptaEZvPsC3ebTLOGS78actBGaTSO",
+	"9xv0n9yhsG/ZQQ5uqxFKYMawLY1HLK/gDJGDIDkY/NxxgzWUS5hk7HNczWg23iplgMYoc7Hz8minTywQ",
+	"TvZ+lfxzsTzm1DpI5oE1mHG5VkYE2Siplm3xcnoEuFOCZJGx536bj6fO/E92miRwlrf3drdv71P+TFSn",
+	"LJpiKCJSRkGujgnAa/ob0+DSYROklUzgaMY6w2VzkCGvIYauUmUcZapUjfNcAq1lzRl0HmKIX01r4hhx",
+	"ywX3xgi+Ddzffwf5TCpqvbZ4brBmDdqfOmOi8Y5E/KUca+fnjfpif+GtQ7/Bs0hpSY/4avyegHF7edRk",
+	"jLaXzCT3OKjiVle98y9mKzJsV1UVWnsU82ybDP4gWKw6w932kVAD0CfcXlVKfeIei1MFxWFKkEIGNZnm",
+	"v6O/fWjlBlkdGp5fGYfnrPzcodn2C8Po9DrqEKyxUC5fUly8hvpxX/N0KtSbUzdklTclCsZbKOEjF8ia",
+	"Fn9o6IfrSomB+7fWT0EOnaHYjXPalkXRMIH1P9dKtlxikUApKe5aWvjokIns5x+zu4d7yOEJjQ0d6cYX",
+	"k0bJNIcS3l0vrhfeRm7jj6BIT4ImVBuds6/i+xpK+BXdHc3TAsMEOt9jl/PC8ZoCB9OMa8LbLgfBnu9D",
+	"e75ZLA6b7jyuF/4NwHO40zb9R6q+Obq+NMdPrjXrWgflIgfBJRed8N8Pi/zweRbKe44olf4MD21CsOdA",
+	"5KXqaW9maFf59B17u1h8pXfs9E3mm4cQjIorTLHgnlg7/dsuD74rXni9e8189/UR+5GHx+6DcfNxpsNj",
+	"nnl3C/9bQdM/BhMp/0RnOD6hn5pVMzafXlCt7IyiD8q6hxB5eVX9tSfY8weUjdtAefuVdd2/tmbEfex8",
+	"xJ66f+uaOcxYes8OAk+feukC81qNr67lanohpXG8ZpYr2rdF85SEHpp7WRStqli7UdaV7xfvbwrq07vV",
+	"7t8AAAD//ylVryWoDgAA",
 }
 
 // GetSwagger returns the Swagger specification corresponding to the generated code
