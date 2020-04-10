938c64cd84f500c5db16055c59b7490088b682e2
James Eagle <jimeagle@gmail.com>
2020-04-10 16:55:19 +0100

    Update stubs

--- pkg/queue/apps.achievements.go
+++ pkg/queue/apps.achievements.go
@@ -1,1 +1,28 @@
 package queue
+
+import (
+	"github.com/Jleagle/rabbit-go"
+	"github.com/gamedb/gamedb/pkg/helpers"
+	"github.com/gamedb/gamedb/pkg/log"
+)
+
+type AppAchievementsMessage struct {
+	ID int `json:"id"`
+}
+
+func appAchievementsHandler(messages []*rabbit.Message) {
+
+	for _, message := range messages {
+
+		payload := AppAchievementsMessage{}
+
+		err := helpers.Unmarshal(message.Message.Body, &payload)
+		if err != nil {
+			log.Err(err, message.Message.Body)
+			sendToFailQueue(message)
+			continue
+		}
+
+		message.Ack(false)
+	}
+}
--- pkg/queue/apps.sameowners.go
+++ pkg/queue/apps.sameowners.go
@@ -1,1 +1,28 @@
 package queue
+
+import (
+	"github.com/Jleagle/rabbit-go"
+	"github.com/gamedb/gamedb/pkg/helpers"
+	"github.com/gamedb/gamedb/pkg/log"
+)
+
+type AppSameownersMessage struct {
+	ID int `json:"id"`
+}
+
+func appSameownersHandler(messages []*rabbit.Message) {
+
+	for _, message := range messages {
+
+		payload := AppSameownersMessage{}
+
+		err := helpers.Unmarshal(message.Message.Body, &payload)
+		if err != nil {
+			log.Err(err, message.Message.Body)
+			sendToFailQueue(message)
+			continue
+		}
+
+		message.Ack(false)
+	}
+}
