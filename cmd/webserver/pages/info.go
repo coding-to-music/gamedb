1fc4f760dcd9dda9b18eb9aba4fd062261509a34
James Eagle <jimeagle@gmail.com>
2020-05-03 17:27:36 +0100

    debug header

--- cmd/webserver/pages/info.go
+++ cmd/webserver/pages/info.go
@@ -3,6 +3,7 @@ package pages
 import (
 	"net/http"
 
+	"github.com/gamedb/gamedb/pkg/log"
 	"github.com/go-chi/chi"
 )
 
@@ -17,6 +18,8 @@ func infoHandler(w http.ResponseWriter, r *http.Request) {
 
 	t := GlobalTemplate{}
 	t.fill(w, r, "Info", "Game DB Information")
+
+	log.Debug(r.Header.Get("cf-ipcountry"))
 
 	returnTemplate(w, r, "info", t)
 }
