a07718d367cd6df2da7aec7299cead5c81b8482f
James Eagle <jimeagle@gmail.com>
2020-05-06 21:11:04 +0100

    Remove count

--- cmd/webserver/pages/webserver.go
+++ cmd/webserver/pages/webserver.go
@@ -182,7 +182,6 @@ type errorTemplate struct {
 
 func getTemplateFuncMap() map[string]interface{} {
 	return template.FuncMap{
-		"add":          func(a int, b int) int { return a + b },
 		"bytes":        func(a uint64) string { return humanize.Bytes(a) },
 		"comma":        func(a int) string { return humanize.Comma(int64(a)) },
 		"comma64":      func(a int64) string { return humanize.Comma(a) },
