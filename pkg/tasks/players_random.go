0c5dbf41fd0e58eaea789850f6e33d87a2b21bc3
James Eagle <jimeagle@gmail.com>
2020-05-09 22:38:16 +0100

    Less random players

--- pkg/tasks/players_random.go
+++ pkg/tasks/players_random.go
@@ -29,7 +29,7 @@ func (c UpdateRandomPlayers) Cron() string {
 
 const (
 	cronInterval = time.Minute
-	playerCount  = 10 // Per consumer
+	playerCount  = 8 // Per consumer
 )
 
 func (c UpdateRandomPlayers) work() (err error) {
