e44b9a796e699c613c4866ddfc9a7377a68215b6
James Eagle <jimeagle@gmail.com>
2020-06-03 19:39:29 +0100

    Add compare games page to header & sitemap

--- cmd/webserver/pages/site_map.go
+++ cmd/webserver/pages/site_map.go
@@ -56,6 +56,7 @@ func SiteMapPagesHandler(w http.ResponseWriter, r *http.Request) {
 		"/experience",
 		"/games",
 		"/games/achievements",
+		"/games/compare",
 		"/games/new-releases",
 		"/games/random",
 		"/games/sales",
