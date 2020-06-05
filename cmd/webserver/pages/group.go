6a1547c8a8cea0d333ba2c3316d35621932b2a69
James Eagle <jimeagle@gmail.com>
2020-06-05 08:22:50 +0100

    Fix error code

--- cmd/webserver/pages/group.go
+++ cmd/webserver/pages/group.go
@@ -47,7 +47,7 @@ func groupHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 
 		if err == mongo.ErrNoDocuments {
-			returnErrorTemplate(w, r, errorTemplate{Code: 400, Message: "Sorry but we can not find this group"})
+			returnErrorTemplate(w, r, errorTemplate{Code: 404, Message: "Sorry but we can not find this group"})
 			return
 		}
 
