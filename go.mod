b08e8b21cd54275395d2c1862c0ab6e7cac1ce27
James Eagle <jimeagle@gmail.com>
2020-06-07 11:22:59 +0100

    Improve article rendering

--- go.mod
+++ go.mod
@@ -105,7 +105,7 @@ require (
 	github.com/yohcop/openid-go v1.0.0
 	go.mongodb.org/mongo-driver v1.3.4
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 	golang.org/x/text v0.3.2
--- pkg/helpers/article.go
+++ pkg/helpers/article.go
@@ -1,10 +1,13 @@
 package helpers
 
 import (
-	"html"
+	"bytes"
 	"html/template"
 	"regexp"
 	"strings"
+
+	"github.com/gamedb/gamedb/pkg/log"
+	"golang.org/x/net/html"
 )
 
 const articleImageBase = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans/"
@@ -14,9 +17,60 @@ var trimArticles = regexp.MustCompile(`(?s)^(<br>)+|(<br>)+$`)
 func GetArticleBody(body string) template.HTML {
 
 	body = strings.ReplaceAll(body, "{STEAM_CLAN_IMAGE}", articleImageBase)
+
+	doc, err := html.Parse(strings.NewReader(body))
+	if err == nil {
+		updateArticleDom(doc)
+		var buf = bytes.NewBufferString("")
+		err := html.Render(buf, doc)
+		log.Err(err)
+		body = buf.String()
+	}
+
 	body = BBCodeCompiler.Compile(body)
 	body = html.UnescapeString(body)
 	body = trimArticles.ReplaceAllString(body, "")
 
 	return template.HTML(body)
+}
+
+func updateArticleDom(n *html.Node) {
+
+	// Remove image heights to make responsive
+	if n.Type == html.ElementNode && n.Data == "img" {
+
+		i := -1
+		for index, attr := range n.Attr {
+			if attr.Key == "height" {
+				i = index
+				break
+			}
+		}
+		if i != -1 {
+			n.Attr = append(n.Attr[:i], n.Attr[i+1:]...)
+		}
+	}
+
+	// Set target on links
+	if n.Type == html.ElementNode && n.Data == "a" {
+
+		var set bool
+		for k, v := range n.Attr {
+			if v.Key == "target" {
+				n.Attr[k].Val = "_blank"
+				set = true
+				break
+			}
+		}
+
+		if !set {
+			n.Attr = append(n.Attr, html.Attribute{Namespace: "", Key: "target", Val: "_blank"})
+			n.Attr = append(n.Attr, html.Attribute{Namespace: "", Key: "rel", Val: "noopener"})
+		}
+	}
+
+	// Recurse
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		updateArticleDom(c)
+	}
 }
