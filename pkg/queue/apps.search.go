816080215f1dc3145c1602eccbcac39fc3d4926b
James Eagle <jimeagle@gmail.com>
2020-06-08 20:02:34 +0100

    Update aliases

--- pkg/queue/apps.search.go
+++ pkg/queue/apps.search.go
@@ -13,28 +13,31 @@ type AppsSearchMessage struct {
 }
 
 var aliases = map[int][]string{
-	10:     {"cs"},
-	70:     {"hl"},
-	220:    {"hl2"},
-	240:    {"css"},
-	440:    {"tf2"},
-	500:    {"l4d"},
-	550:    {"l4d2"},
-	570:    {"dota", "dota2"},
-	730:    {"csgo", "cs:go"},
-	8980:   {"bl"},
-	4000:   {"gm"},
-	49520:  {"bl2"},
-	8500:   {"eve"},
-	218620: {"pd2"},
-	238960: {"poe"},
-	252950: {"rl"},
-	271590: {"gta5"},
-	346110: {"ark"},
-	359550: {"r6"},
-	397540: {"bl3"},
-	546560: {"hla"},
-	985890: {"sor", "sor4"},
+	346110: {"ark"},           // ARK: Survival Evolved
+	49520:  {"bl2"},           // Borderlands 2
+	397540: {"bl3"},           // Borderlands 3
+	8980:   {"bl"},            // Borderlands GOTY
+	10:     {"cs"},            // Counter-Strike
+	730:    {"csgo", "cs:go"}, // Counter-Strike: Global Offensive
+	240:    {"css"},           // Counter-Strike: Source
+	570:    {"dota", "dota2"}, // Dota 2
+	8500:   {"eve"},           // EVE Online
+	4000:   {"gm"},            // Garry's Mod
+	271590: {"gta5"},          // Grand Theft Auto V
+	70:     {"hl"},            // Half-Life
+	546560: {"hla"},           // Half-Life: Alyx
+	220:    {"hl2"},           // Half-Life 2
+	500:    {"l4d"},           // Left 4 Dead
+	550:    {"l4d2"},          // Left 4 Dead 2
+	238960: {"poe"},           // Path of Exile
+	218620: {"pd2"},           // PAYDAY 2
+	252950: {"rl"},            // Rocket League
+	3900:   {"civ", "civ4"},   // Sid Meier's Civilization IV
+	8930:   {"civ", "civ5"},   // Sid Meier's Civilization V
+	289070: {"civ", "civ6"},   // Sid Meier's Civilization VI
+	985890: {"sor", "sor4"},   // Streets of Rage 4
+	440:    {"tf2"},           // Team Fortress 2
+	359550: {"r6"},            // Tom Clancy's Rainbow Six Siege
 }
 
 func appsSearchHandler(messages []*rabbit.Message) {
