e37a753aac62ba411e6b8c6d1260da3105a4b840
James Eagle <jimeagle@gmail.com>
2020-06-19 21:05:49 +0100

    Show donations

---
+++ cmd/webserver/assets/js/donate.js
@@ -0,0 +1,35 @@
+if ($('#donate-page').length > 0) {
+
+    const options = {
+        "order": [[1, 'desc']],
+        "createdRow": function (row, data, dataIndex) {
+            if (data[0] > 0) {
+                $(row).attr('data-link', data[2]);
+            }
+        },
+        "columnDefs": [
+            // Icon / Player Name
+            {
+                "targets": 0,
+                "render": function (data, type, row) {
+                    return '<div class="icon-name"><div class="icon"><img data-lazy="' + row[3] + '" alt="" data-lazy-alt="' + row[1] + '"></div><div class="name">' + row[1] + '</div></div>'
+                },
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    $(td).addClass('img');
+                },
+                "orderable": false,
+            },
+            // Donation
+            {
+                "targets": 1,
+                "render": function (data, type, row) {
+                    return '$' + row[5].toLocaleString();
+                },
+                "orderable": false,
+            },
+        ]
+    };
+
+    $('#top-donations').gdbTable({tableOptions: options});
+    $('#new-donations').gdbTable({tableOptions: options});
+}
--- cmd/webserver/pages/donate.go
+++ cmd/webserver/pages/donate.go
@@ -3,14 +3,20 @@ package pages
 import (
 	"net/http"
 
+	"github.com/gamedb/gamedb/cmd/webserver/pages/helpers/datatable"
+	"github.com/gamedb/gamedb/pkg/log"
+	"github.com/gamedb/gamedb/pkg/mongo"
 	"github.com/gamedb/gamedb/pkg/sql"
 	"github.com/go-chi/chi"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func DonateRouter() http.Handler {
 
 	r := chi.NewRouter()
 	r.Get("/", donateHandler)
+	r.Get("/top.json", topDonateHandler)
+	r.Get("/latest.json", latestDonateHandler)
 	return r
 }
 
@@ -32,4 +38,112 @@ func donateHandler(w http.ResponseWriter, r *http.Request) {
 type donateTemplate struct {
 	GlobalTemplate
 	Pages map[int]int
+}
+
+func topDonateHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Get donators
+	donators, err := sql.TopDonators()
+	if err != nil {
+		log.Err(err)
+		return
+	}
+
+	// Get players
+	var playerIDs []int64
+	for _, v := range donators {
+		playerIDs = append(playerIDs, v.PlayerID)
+	}
+
+	var playersMap = map[int64]mongo.Player{}
+	players, err := mongo.GetPlayersByID(playerIDs, bson.M{"_id": 1, "persona_name": 1, "avatar": 1, "country_code": 1})
+	if err != nil {
+		log.Err(err)
+		return
+	}
+
+	for _, v := range players {
+		playersMap[v.ID] = v
+	}
+
+	// Response
+	var query = datatable.NewDataTableQuery(r, false)
+	var response = datatable.NewDataTablesResponse(r, query, 0, 0, nil)
+	for _, donator := range donators {
+
+		var player mongo.Player
+		if val, ok := playersMap[donator.PlayerID]; ok {
+			player = val
+		} else {
+			player = mongo.Player{
+				PersonaName: "Anonymous",
+			}
+		}
+
+		// Also update below
+		response.AddRow([]interface{}{
+			player.ID,          // 0
+			player.GetName(),   // 1
+			player.GetPath(),   // 2
+			player.GetAvatar(), // 3
+			player.GetFlag(),   // 4
+			donator.Format(),   // 5
+		})
+	}
+
+	returnJSON(w, r, response)
+}
+
+func latestDonateHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Get donations
+	donations, err := sql.LatestDonations()
+	if err != nil {
+		log.Err(err)
+		return
+	}
+
+	// Get players
+	var playerIDs []int64
+	for _, v := range donations {
+		playerIDs = append(playerIDs, v.PlayerID)
+	}
+
+	var playersMap = map[int64]mongo.Player{}
+	players, err := mongo.GetPlayersByID(playerIDs, bson.M{"_id": 1, "persona_name": 1, "avatar": 1, "country_code": 1})
+	if err != nil {
+		log.Err(err)
+		return
+	}
+
+	for _, v := range players {
+		playersMap[v.ID] = v
+	}
+
+	// Response
+	var query = datatable.NewDataTableQuery(r, false)
+	var response = datatable.NewDataTablesResponse(r, query, 0, 0, nil)
+	for _, donation := range donations {
+
+		var player mongo.Player
+		if val, ok := playersMap[donation.PlayerID]; ok {
+			player = val
+		} else {
+			player = mongo.Player{
+				PersonaName: "Anonymous",
+			}
+		}
+
+		// Also update above
+		response.AddRow([]interface{}{
+			player.ID,          // 0
+			player.GetName(),   // 1
+			player.GetPath(),   // 2
+			player.GetAvatar(), // 3
+			player.GetFlag(),   // 4
+			donation.Format(),  // 5
+		})
+	}
+
+	returnJSON(w, r, response)
 }
