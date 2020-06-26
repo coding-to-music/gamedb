303d826e1c2844392c37529c441c5f234e43904a
James Eagle <jimeagle@gmail.com>
2020-06-26 23:44:02 +0100

    Update sorting

--- cmd/webserver/pages/achievements.go
+++ cmd/webserver/pages/achievements.go
@@ -43,7 +43,7 @@ func achievementsAjaxHandler(w http.ResponseWriter, r *http.Request) {
 
 		var err error
 		var sorters = query.GetOrderElastic(map[string]string{
-			"1": "completed, app_owners x",
+			"1": "completed, app_owners desc",
 		})
 
 		achievements, filtered, err = elasticHelpers.SearchAppAchievements(query.GetOffset(), search, sorters)
