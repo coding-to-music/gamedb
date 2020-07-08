7ff1590d503ed041beaa07c63f623b4f9983e1b8
James Eagle <jimeagle@gmail.com>
2020-07-08 13:19:18 +0100

    Update apps search

--- pkg/elastic-search/apps.go
+++ pkg/elastic-search/apps.go
@@ -86,7 +86,7 @@ func SearchApps(limit int, offset int, search string, sorters []elastic.Sorter, 
 			Must(
 				elastic.NewBoolQuery().MinimumNumberShouldMatch(1).Should(
 					elastic.NewTermQuery("id", search).Boost(5),
-					elastic.NewMatchQuery("name", search).Boost(2),
+					elastic.NewMatchQuery("name", search).Boost(1),
 					elastic.NewTermQuery("aliases", search2).Boost(1),
 					elastic.NewPrefixQuery("name", search).Boost(0.2),
 				),
