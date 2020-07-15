843f6a70462d9cb00b0434dc29946a1fa5f76a42
James Eagle <jimeagle@gmail.com>
2020-07-15 08:37:09 +0100

    Save youtube video count

--- pkg/queue/apps.youtube.go
+++ pkg/queue/apps.youtube.go
@@ -3,7 +3,6 @@ package queue
 import (
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/Jleagle/rabbit-go"
@@ -33,126 +32,85 @@ func appYoutubeHandler(messages []*rabbit.Message) {
 			continue
 		}
 
-		var wg sync.WaitGroup
+		payload.Name = strings.TrimSpace(payload.Name)
 
-		wg.Add(1)
-		var youtubeViews uint64
-		var youtubeComments uint64
-		go func() {
-
-			defer wg.Done()
-
-			var err error
-			youtubeViews, youtubeComments, err = appsYoutubeFetch(payload)
-			if err != nil {
-				log.Err(err, payload.ID)
-				sendToRetryQueue(message)
-				return
-			}
-		}()
-
-		wg.Wait()
-
-		if message.ActionTaken {
+		if payload.Name == "" {
+			message.Ack(false)
 			continue
 		}
 
+		// `part` can be:
+		// id, snippet, contentDetails, fileDetails, player, processingDetails, recordingDetails, statistics, status, suggestions, topicDetails
+
+		// Get video IDs from search
+		searchRequest := youtube.YoutubeService.Search.List([]string{"id"}).
+			Context(youtube.YoutubeContext).
+			MaxResults(50).
+			SafeSearch("none").
+			Type("video").
+			Q(payload.Name).
+			Order("viewCount").
+			PublishedAfter(time.Now().Add(-time.Hour * 24).Format(time.RFC3339))
+
+		searchResponse, err := searchRequest.Do()
+		if err != nil {
+			log.Err(err, payload.ID)
+			sendToRetryQueue(message)
+			continue
+		}
+
+		var ids []string
+		for _, v := range searchResponse.Items {
+			ids = append(ids, v.Id.VideoId)
+		}
+
+		// Get video statistics from IDs
+		listRequest := youtube.YoutubeService.Videos.
+			List([]string{"statistics"}).
+			Id(strings.Join(ids, ","))
+
+		listResponse, err := listRequest.Do()
+		if err != nil {
+			log.Err(err, payload.ID)
+			sendToRetryQueue(message)
+			continue
+		}
+
+		var views uint64
+		var comments uint64
+		for _, v := range listResponse.Items {
+			views += v.Statistics.ViewCount
+			comments += v.Statistics.CommentCount
+		}
+
 		// Save to Influx
-		wg.Add(1)
-		go func() {
+		if views > 0 || comments > 0 {
 
-			defer wg.Done()
+			point := influx.Point{
+				Measurement: string(influxHelper.InfluxMeasurementApps),
+				Tags: map[string]string{
+					"app_id": strconv.Itoa(payload.ID),
+				},
+				Fields: map[string]interface{}{
+					"youtube_views":    int64(views),
+					"youtube_comments": int64(comments),
+					"youtube_videos":   searchResponse.PageInfo.TotalResults,
+				},
+				Time:      time.Now(),
+				Precision: "h",
+			}
 
-			err = appsYoutubeSave(payload, youtubeViews, youtubeComments)
+			_, err = influxHelper.InfluxWrite(influxHelper.InfluxRetentionPolicyAllTime, point)
 			if err != nil {
 				log.Err(err, payload.ID)
+				if val, ok := err.(*googleapi.Error); ok && val.Code == 403 {
+					time.Sleep(time.Minute)
+				}
 				sendToRetryQueue(message)
-				return
+				continue
 			}
-		}()
-
-		wg.Wait()
-
-		if message.ActionTaken {
-			continue
 		}
 
 		message.Ack(false)
 	}
-}
-
-func appsYoutubeFetch(payload AppYoutubeMessage) (uint64, uint64, error) {
-
-	payload.Name = strings.TrimSpace(payload.Name)
-	if payload.Name == "" {
-		return 0, 0, nil
-	}
-
-	// `part` can be:
-	// id, snippet, contentDetails, fileDetails, player, processingDetails, recordingDetails, statistics, status, suggestions, topicDetails
-
-	// Get video IDs from search
-	searchResponse, err := youtube.YoutubeService.Search.List([]string{"id"}).
-		Context(youtube.YoutubeContext).
-		MaxResults(50).
-		SafeSearch("none").
-		Type("video").
-		Q(payload.Name).
-		Order("viewCount").
-		PublishedAfter(time.Now().Add(-time.Hour * 24).Format(time.RFC3339)).
-		Do()
-
-	if err != nil {
-		return 0, 0, err
-	}
-
-	var ids []string
-	for _, v := range searchResponse.Items {
-		ids = append(ids, v.Id.VideoId)
-	}
-
-	// Get video statistics from IDs
-	listResponse, err := youtube.YoutubeService.Videos.
-		List([]string{"statistics"}).
-		Id(strings.Join(ids, ",")).
-		Do()
-
-	if err != nil {
-		return 0, 0, err
-	}
-
-	var views uint64
-	var comments uint64
-	for _, v := range listResponse.Items {
-		views += v.Statistics.ViewCount
-		comments += v.Statistics.CommentCount
-	}
-
-	//
-	return views, comments, nil
-}
-
-func appsYoutubeSave(payload AppYoutubeMessage, views uint64, comments uint64) (err error) {
-
-	if views > 0 || comments > 0 {
-
-		_, err = influxHelper.InfluxWrite(influxHelper.InfluxRetentionPolicyAllTime, influx.Point{
-			Measurement: string(influxHelper.InfluxMeasurementApps),
-			Tags: map[string]string{
-				"app_id": strconv.Itoa(payload.ID),
-			},
-			Fields: map[string]interface{}{
-				"youtube_views":    int64(views),
-				"youtube_comments": int64(comments),
-			},
-			Time:      time.Now(),
-			Precision: "h",
-		})
-	}
-
-	if val, ok := err.(*googleapi.Error); ok && val.Code == 403 {
-		time.Sleep(time.Minute)
-	}
-
-	return err
 }
