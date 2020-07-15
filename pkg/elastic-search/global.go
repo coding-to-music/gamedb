d24f2adb75d2a73a14876caf4a5e161f06a2636c
James Eagle <jimeagle@gmail.com>
2020-07-15 20:30:40 +0100

    Commit global search

--- pkg/elastic-search/global.go
+++ pkg/elastic-search/global.go
@@ -14,18 +14,17 @@ const (
 	GlobalTypeApp         = "app"
 	GlobalTypeArticle     = "article"
 	GlobalTypeGroup       = "group"
-	GlobalTypePackage     = "package"
 	GlobalTypePlayer      = "player"
 )
 
 type Global struct {
-	ID         string  `json:"id"` // String as can contain mixed types
-	Name       string  `json:"name"`
-	NameMarked string  `json:"name_marked"`
-	Icon       string  `json:"icon"`
-	AppID      int     `json:"app_id"`
-	Type       string  `json:"type"`
-	Score      float64 `json:"-"`
+	ID         string `json:"id"` // String as can contain mixed types
+	Name       string `json:"name"`
+	NameMarked string `json:"name_marked"`
+	Icon       string `json:"icon"`
+	// AppID      int     `json:"app_id"`
+	Type  string  `json:"type"`
+	Score float64 `json:"-"`
 }
 
 func (global Global) GetName() string {
@@ -47,6 +46,28 @@ func (global Global) GetName() string {
 
 	default:
 		return global.Name
+	}
+}
+
+func (global Global) GetNameMarked() string {
+
+	switch global.Type {
+	case GlobalTypeApp:
+
+		i, _ := strconv.Atoi(global.ID)
+		return helpers.GetAppName(i, global.NameMarked)
+
+	case GlobalTypePlayer:
+
+		i64, _ := strconv.ParseInt(global.ID, 10, 64)
+		return helpers.GetPlayerName(i64, global.NameMarked)
+
+	case GlobalTypeGroup:
+
+		return helpers.GetGroupName(global.ID, global.NameMarked)
+
+	default:
+		return global.NameMarked
 	}
 }
 
@@ -93,18 +114,17 @@ func (global Global) GetPath() string {
 	}
 }
 
-func SearchGlobal(limit int, offset int, search string, sorters []elastic.Sorter) (apps []App, total int64, err error) {
+func SearchGlobal(limit int, offset int, search string) (items []Global, aggregations map[string]map[string]int64, total int64, err error) {
 
 	client, ctx, err := GetElastic()
 	if err != nil {
-		return apps, 0, err
+		return items, aggregations, 0, err
 	}
 
 	searchService := client.Search().
 		Index(IndexGlobal).
 		From(offset).
-		Aggregation("type", elastic.NewTermsAggregation().Field("type").Size(10).OrderByCountDesc()).
-		Highlight(elastic.NewHighlight().Field("name").PreTags("<mark>").PostTags("</mark>")).
+		// Aggregation("type", elastic.NewTermsAggregation().Field("type").Size(10).OrderByCountDesc()).
 		TrackTotalHits(true).
 		Size(limit)
 
@@ -112,59 +132,54 @@ func SearchGlobal(limit int, offset int, search string, sorters []elastic.Sorter
 
 		var search2 = helpers.RegexNonAlphaNumeric.ReplaceAllString(search, "")
 
-		searchService.Query(elastic.NewBoolQuery().
-			Must(
-				elastic.NewBoolQuery().MinimumNumberShouldMatch(1).Should(
-					elastic.NewTermQuery("id", search2).Boost(5),
-					elastic.NewMatchQuery("name", search).Boost(1),
-					elastic.NewPrefixQuery("name", search).Boost(0.2),
-				),
-			).
-			Should(
-				elastic.NewTermQuery("type", GlobalTypePlayer).Boost(1.2),
-				elastic.NewTermQuery("type", GlobalTypeApp).Boost(1.1),
+		searchService.Query(
+			elastic.NewBoolQuery().MinimumNumberShouldMatch(1).Should(
+				elastic.NewTermQuery("id", search2).Boost(5),
+				elastic.NewMatchQuery("name", search).Boost(1),
+				elastic.NewPrefixQuery("name", search).Boost(0.2),
 			),
 		)
+
+		searchService.Highlight(elastic.NewHighlight().Field("name").PreTags("<mark>").PostTags("</mark>"))
 	}
 
 	searchResult, err := searchService.Do(ctx)
 	if err != nil {
-		return apps, 0, err
-	}
-
-	if len(sorters) > 0 {
-		searchService.SortBy(sorters...)
+		return items, aggregations, 0, err
 	}
 
 	for _, hit := range searchResult.Hits.Hits {
 
-		var app = App{}
+		var item = Global{}
 
-		err := json.Unmarshal(hit.Source, &app)
+		err := json.Unmarshal(hit.Source, &item)
 		if err != nil {
 			log.Err(err)
 			continue
 		}
 
 		if hit.Score != nil {
-			app.Score = *hit.Score
+			item.Score = *hit.Score
 		}
 
-		app.NameMarked = app.Name
+		item.NameMarked = item.Name
 		if val, ok := hit.Highlight["name"]; ok {
 			if len(val) > 0 {
-				app.NameMarked = val[0]
+				item.NameMarked = val[0]
 			}
 		}
 
-		apps = append(apps, app)
+		items = append(items, item)
 	}
 
-	return apps, searchResult.TotalHits(), err
+	return items, aggregations, searchResult.TotalHits(), err
 }
 
-func IndexGlobalItem(global Global) error {
-	return indexDocument(IndexGlobal, global.Type+"-"+global.ID, global)
+func IndexGlobalItem(item Global) error {
+	if item.Name == "" {
+		return nil
+	}
+	return indexDocument(IndexGlobal, item.Type+"-"+item.ID, item)
 }
 
 //noinspection GoUnusedExportedFunction
@@ -177,7 +192,7 @@ func DeleteAndRebuildGlobalIndex() {
 				"id":     fieldTypeKeyword,
 				"name":   fieldTypeText,
 				"icon":   fieldTypeDisabled,
-				"app_id": fieldTypeKeyword,
+				"app_id": fieldTypeInteger,
 				"type":   fieldTypeKeyword,
 			},
 		},
