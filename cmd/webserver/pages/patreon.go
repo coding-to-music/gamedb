7a42b4509746ab939aee100a530fd01eeacf7c6c
James Eagle <jimeagle@gmail.com>
2020-07-17 21:30:53 +0100

    Save webhooks even if they dont parse

--- cmd/webserver/pages/patreon.go
+++ cmd/webserver/pages/patreon.go
@@ -2,7 +2,6 @@ package pages
 
 import (
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/Jleagle/patreon-go/patreon"
@@ -31,16 +30,18 @@ func patreonWebhookPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	_, err = mongo.InsertOne(mongo.CollectionPatreonWebhooks, mongo.PatreonWebhook{
+		CreatedAt:   time.Now(),
+		RequestBody: string(b),
+		Event:       event,
+	})
+	if err != nil {
+		log.Err(err, r)
+	}
+
 	pwr, err := patreon.UnmarshalBytes(b)
 	if err != nil {
 		log.Err(err, r, b)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	err = saveWebhookToMongo(event, pwr, b)
-	if err != nil {
-		log.Err(err, r)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -51,26 +52,6 @@ func patreonWebhookPostHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-}
-
-func saveWebhookToMongo(event string, pwr patreon.Webhook, body []byte) (err error) {
-
-	userId, err := strconv.Atoi(pwr.User.ID)
-	log.Err(err)
-
-	_, err = mongo.InsertOne(mongo.CollectionPatreonWebhooks, mongo.PatreonWebhook{
-		CreatedAt:                   time.Now(),
-		RequestBody:                 string(body),
-		Event:                       event,
-		UserID:                      userId,
-		UserEmail:                   pwr.User.Attributes.Email,
-		DataPatronStatus:            pwr.Data.Attributes.PatronStatus,
-		DataLifetimeSupportCents:    pwr.Data.Attributes.LifetimeSupportCents,
-		DataPledgeAmountCents:       pwr.Data.Attributes.PledgeAmountCents,
-		DataPledgeCapAmountCents:    int(pwr.Data.Attributes.PledgeCapAmountCents),
-		DataPledgeRelationshipStart: pwr.Data.Attributes.PledgeRelationshipStart,
-	})
-	return err
 }
 
 func saveWebhookEvent(r *http.Request, event mongo.EventEnum, pwr patreon.Webhook) (err error) {
--- pkg/mongo/patreon_webhooks.go
+++ pkg/mongo/patreon_webhooks.go
@@ -10,16 +10,9 @@ import (
 )
 
 type PatreonWebhook struct {
-	CreatedAt                   time.Time `bson:"created_at"`
-	RequestBody                 string    `bson:"request_body"`
-	Event                       string    `bson:"event"`
-	UserID                      int       `json:"user_id"`
-	UserEmail                   string    `bson:"user_email"`
-	DataPatronStatus            string    `bson:"patron_status"`
-	DataLifetimeSupportCents    int       `bson:"lifetime_support_cents"`
-	DataPledgeAmountCents       int       `bson:"pledge_amount_cents"`
-	DataPledgeCapAmountCents    int       `bson:"pledge_cap_amount_cents"`
-	DataPledgeRelationshipStart time.Time `bson:"pledge_relationship_start"`
+	CreatedAt   time.Time `bson:"created_at"`
+	RequestBody string    `bson:"request_body"`
+	Event       string    `bson:"event"`
 }
 
 func (webhook PatreonWebhook) BSON() bson.D {
@@ -28,20 +21,13 @@ func (webhook PatreonWebhook) BSON() bson.D {
 		{"created_at", webhook.CreatedAt},
 		{"request_body", webhook.RequestBody},
 		{"event", webhook.Event},
-		{"user_id", webhook.UserID},
-		{"user_email", webhook.UserEmail},
-		{"data_lifetime_support_cents", webhook.DataLifetimeSupportCents},
-		{"data_patron_status", webhook.DataPatronStatus},
-		{"data_pledge_amount_cents", webhook.DataPledgeAmountCents},
-		{"data_pledge_cap_amount_cents", webhook.DataPledgeCapAmountCents},
-		{"data_pledge_relationship_start", webhook.DataPledgeRelationshipStart},
 	}
 }
 
-func (webhook PatreonWebhook) Raw() (raw patreon.Webhook, err error) {
+func (webhook PatreonWebhook) Unmarshal() (wh patreon.Webhook, err error) {
 
-	err = helpers.Unmarshal([]byte(webhook.RequestBody), &raw)
-	return raw, err
+	err = helpers.Unmarshal([]byte(webhook.RequestBody), &wh)
+	return wh, err
 }
 
 func GetPatreonWebhooks(offset int64, limit int64, sort bson.D, filter bson.D, projection bson.M) (webhooks []PatreonWebhook, err error) {
