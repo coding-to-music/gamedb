2758d8b5d8a836aae53ff8debf1736c25252a29e
James Eagle <jimeagle@gmail.com>
2020-08-24 19:52:44 +0100

    Remove panic

--- pkg/helpers/general.go
+++ pkg/helpers/general.go
@@ -10,12 +10,8 @@ type TupleInt struct {
 	Value int `json:"v"`
 }
 
-// Ignore returns nil if an error is of one of the provided types, returns the provided error otherwise.
+// IgnoreErrors returns nil if an error is of one of the provided types, returns the provided error otherwise.
 func IgnoreErrors(err error, errs ...error) error {
-
-	if len(errs) == 0 {
-		panic("Using IgnoreErrors wrong")
-	}
 
 	for _, v := range errs {
 		if err == v {
