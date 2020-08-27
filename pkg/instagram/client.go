13af8978c45b7d76759e200cf5070ab9318863e9
James Eagle <jimeagle@gmail.com>
2020-08-27 22:33:14 +0100

    Add lock to ig client

--- pkg/instagram/client.go
+++ pkg/instagram/client.go
@@ -1,23 +1,25 @@
 package instagram
 
 import (
+	"sync"
+
 	"github.com/ahmdrz/goinsta/v2"
 	"github.com/gamedb/gamedb/pkg/config"
 )
 
 var (
 	instagram *goinsta.Instagram
+	lock      sync.Mutex
 )
 
 func getInstagram() (*goinsta.Instagram, error) {
 
+	lock.Lock()
+	defer lock.Unlock()
+
 	if instagram == nil {
 
-		client := goinsta.New(
-			config.C.InstagramUsername,
-			config.C.InstagramPassword,
-		)
-
+		client := goinsta.New(config.C.InstagramUsername, config.C.InstagramPassword)
 		err := client.Login()
 		if err != nil {
 			return client, err
