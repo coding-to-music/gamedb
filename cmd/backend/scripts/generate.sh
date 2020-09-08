36f667de4b9bb07e7ae25349840361a75881045e
James Eagle <jimeagle@gmail.com>
2020-09-08 12:50:28 +0100

    Fix protoc script

--- cmd/backend/scripts/generate.sh
+++ cmd/backend/scripts/generate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 protoc \
+  --go_opt=paths=source_relative \
   --proto_path ../../pkg/backend/protos \
   --go_out=plugins=grpc:../../pkg/backend/generated \
   ../../pkg/backend/protos/*.proto
