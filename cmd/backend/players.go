36f458321297254387d7ba8c8a4d50c423d472c8
James Eagle <jimeagle@gmail.com>
2020-09-11 14:45:48 +0100

    Merge files

--- cmd/backend/players.go
+++ cmd/backend/players.go
@@ -12,3 +12,7 @@ type PlayersServer struct {
 func (p PlayersServer) List(ctx context.Context, request *generated.ListPlayersRequest) (*generated.PlayersMongoResponse, error) {
 	panic("implement me")
 }
+
+func (p PlayersServer) Search(ctx context.Context, request *generated.SearchPlayersRequest) (*generated.PlayersElasticResponse, error) {
+	panic("implement me")
+}
