40e4e5424712e2c51d911601f11bc0957c150888
James Eagle <jimeagle@gmail.com>
2020-09-23 13:10:18 +0100

    Fix using cloudflare header

--- cmd/frontend/helpers/middleware/middleware.go
+++ cmd/frontend/helpers/middleware/middleware.go
@@ -30,10 +30,10 @@ func MiddlewareRealIP(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		cf := r.Header.Get("cf-connecting-ip")
-		nginx := r.Header.Get("X-Real-IP")
+		nginx := r.Header.Get("x-real-ip")
 
 		if cf != "" {
-			r.RemoteAddr = nginx
+			r.RemoteAddr = cf
 		} else if nginx != "" {
 			r.RemoteAddr = nginx
 		}
