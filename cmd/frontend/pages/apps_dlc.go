6f4a021f0ae909ad4963bf149901f39509c7cbbb
James Eagle <jimeagle@gmail.com>
2020-11-10 22:39:38 +0000

    auth check

--- cmd/frontend/pages/apps_dlc.go
+++ cmd/frontend/pages/apps_dlc.go
@@ -39,81 +39,84 @@ func appsDLCAjaxHandler(w http.ResponseWriter, r *http.Request) {
 
 	var playerApps []mongo.PlayerApp
 	var playerAppsOwned = map[int]int{}
+	var count int64
 
 	playerID := session.GetPlayerIDFromSesion(r)
-	filter := bson.D{
-		{"player_id", playerID}, // Needed for count
-		{"app_dlc_count", bson.M{"$gt": 0}},
+	if playerID > 0 {
+
+		filter := bson.D{
+			{"player_id", playerID}, // Needed for count
+			{"app_dlc_count", bson.M{"$gt": 0}},
+		}
+
+		wg.Add(1)
+		go func() {
+
+			defer wg.Done()
+
+			var err error
+			var columns = map[string]string{
+				"0": "app_name",
+				"1": "app_dlc_count",
+			}
+
+			playerApps, err = mongo.GetPlayerAppsByPlayer(playerID, query.GetOffset64(), 100, query.GetOrderMongo(columns), nil, filter)
+			if err != nil {
+				log.ErrS(err)
+				return
+			}
+
+			// Get all DLC for page
+			var appIDs []int
+			var appIDs2 bson.A
+			for _, v := range playerApps {
+				appIDs = append(appIDs, v.AppID)
+				appIDs2 = append(appIDs2, v.AppID)
+			}
+
+			dlcs, err := mongo.GetDLCForApps(appIDs, 0, 0, nil, nil, bson.M{"app_id": 1, "dlc_id": 1})
+			if err != nil {
+				log.ErrS(err)
+				return
+			}
+
+			// Get owned DLC
+			var dlcsMap = map[int]int{}
+			var dlcAppIDs bson.A
+			for _, v := range dlcs {
+				dlcAppIDs = append(dlcAppIDs, v.DLCID)
+				dlcsMap[v.DLCID] = v.AppID
+			}
+
+			filter = bson.D{{"app_id", bson.M{"$in": dlcAppIDs}}}
+
+			owned, err := mongo.GetPlayerAppsByPlayer(playerID, 0, 0, nil, bson.M{"_id": 1, "app_id": 1}, filter)
+			if err != nil {
+				log.ErrS(err)
+				return
+			}
+
+			for _, v := range owned {
+				playerAppsOwned[dlcsMap[v.AppID]]++
+			}
+		}()
+
+		// Get count
+		wg.Add(1)
+		go func() {
+
+			defer wg.Done()
+
+			var err error
+			count, err = mongo.CountDocuments(mongo.CollectionPlayerApps, filter, 0)
+			if err != nil {
+				log.ErrS(err)
+			}
+		}()
+
+		// Wait
+		wg.Wait()
 	}
-
-	wg.Add(1)
-	go func() {
-
-		defer wg.Done()
-
-		var err error
-		var columns = map[string]string{
-			"0": "app_name",
-			"1": "app_dlc_count",
-		}
-
-		playerApps, err = mongo.GetPlayerAppsByPlayer(playerID, query.GetOffset64(), 100, query.GetOrderMongo(columns), nil, filter)
-		if err != nil {
-			log.ErrS(err)
-			return
-		}
-
-		// Get all DLC for page
-		var appIDs []int
-		var appIDs2 bson.A
-		for _, v := range playerApps {
-			appIDs = append(appIDs, v.AppID)
-			appIDs2 = append(appIDs2, v.AppID)
-		}
-
-		dlcs, err := mongo.GetDLCForApps(appIDs, 0, 0, nil, nil, bson.M{"app_id": 1, "dlc_id": 1})
-		if err != nil {
-			log.ErrS(err)
-			return
-		}
-
-		// Get owned DLC
-		var dlcsMap = map[int]int{}
-		var dlcAppIDs bson.A
-		for _, v := range dlcs {
-			dlcAppIDs = append(dlcAppIDs, v.DLCID)
-			dlcsMap[v.DLCID] = v.AppID
-		}
-
-		filter = bson.D{{"app_id", bson.M{"$in": dlcAppIDs}}}
-
-		owned, err := mongo.GetPlayerAppsByPlayer(playerID, 0, 0, nil, bson.M{"_id": 1, "app_id": 1}, filter)
-		if err != nil {
-			log.ErrS(err)
-			return
-		}
-
-		for _, v := range owned {
-			playerAppsOwned[dlcsMap[v.AppID]]++
-		}
-	}()
-
-	// Get count
-	var count int64
-	wg.Add(1)
-	go func() {
-
-		defer wg.Done()
-
-		var err error
-		count, err = mongo.CountDocuments(mongo.CollectionPlayerApps, filter, 0)
-		if err != nil {
-			log.ErrS(err)
-		}
-	}()
-
-	// Wait
-	wg.Wait()
 
 	var response = datatable.NewDataTablesResponse(r, query, count, count, nil)
 	for _, playerApp := range playerApps {
