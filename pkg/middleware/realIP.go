9956affa29b72833691cf7769fc6acdf87d8da7a
James Eagle <jimeagle@gmail.com>
2020-11-29 10:59:25 +0000

    Improve real ip middleware

--- pkg/middleware/realIP.go
+++ pkg/middleware/realIP.go
@@ -2,18 +2,29 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/gamedb/gamedb/pkg/helpers"
 )
 
 func MiddlewareRealIP(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		cf := r.Header.Get("cf-connecting-ip")
-		nginx := r.Header.Get("x-real-ip")
+		headers := []string{
+			r.Header.Get("CF-Connecting-IP"), // Cloudflare
+			r.Header.Get("X-Real-IP"),        // Nginx
+			r.Header.Get("X-Client-IP"),      //
+			r.Header.Get("X-Forwarded-For"),  //
+			r.Header.Get("X-Forwarded"),      // Variation
+		}
 
-		if cf != "" {
-			r.RemoteAddr = cf
-		} else if nginx != "" {
-			r.RemoteAddr = nginx
+		for _, v := range headers {
+			if v != "" {
+				ip := helpers.RegexIP.FindString(v)
+				if ip != "" {
+					r.RemoteAddr = ip
+					break
+				}
+			}
 		}
 
 		next.ServeHTTP(w, r)
