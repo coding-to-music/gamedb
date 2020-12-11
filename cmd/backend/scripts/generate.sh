c77fa442a2fae4a0aaf2d4a87f3cd5f48bb95c26
Jleagle <jimeagle@gmail.com>
2020-12-11 22:33:27 +0000

    Update protoc caommand

--- cmd/backend/scripts/generate.sh
+++ cmd/backend/scripts/generate.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-BASE=/Users/james/Websites/gamedb/gamedb
-
 protoc \
-  -I $BASE/pkg/backend/protos \
-  --go_out=$BASE/pkg/backend/generated \
+  -I "$STEAM_PATH"/pkg/backend/protos \
+  --go_out="$STEAM_PATH"/pkg/backend/generated \
   --go_opt=paths=source_relative \
-  --go-grpc_out=$BASE/pkg/backend/generated \
+  --go-grpc_out="$STEAM_PATH"/pkg/backend/generated \
   --go-grpc_opt=paths=source_relative \
-  --grpc-gateway_out $BASE/pkg/backend/generated \
+  --grpc-gateway_out "$STEAM_PATH"/pkg/backend/generated \
   --grpc-gateway_opt logtostderr=true \
   --grpc-gateway_opt paths=source_relative \
   --grpc-gateway_opt generate_unbound_methods=true \
-  --openapiv2_out $BASE/cmd/api/generated_openapi \
+  --openapiv2_out "$STEAM_PATH"/cmd/api/generated_openapi \
   --openapiv2_opt logtostderr=true \
-  $BASE/pkg/backend/protos/*.proto
+  "$STEAM_PATH"/pkg/backend/protos/*.proto
 
 echo "Done"
