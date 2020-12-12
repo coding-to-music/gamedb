3ffb76edd4c324cb022e6cce237ce69bf164f824
James Eagle <jimeagle@gmail.com>
2020-12-12 20:24:52 +0000

    Add dependency checker

--- cmd/backend/scripts/generate.sh
+++ cmd/backend/scripts/generate.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+files=(
+  "google.golang.org/protobuf/cmd/protoc-gen-go"
+  "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+  "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+  "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+)
+
+for i in "${files[@]}"; do
+  if [ ! -f $(which $(basename $i)) ]; then
+    go get -u "$i"
+  fi
+done
+
 protoc \
   -I "$STEAM_PATH"/pkg/backend/protos \
   --go_out="$STEAM_PATH"/pkg/backend/generated \
