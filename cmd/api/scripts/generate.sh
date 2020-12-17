4460f3a9264c2c1e4cf4a38d8baa76cd0ee51b5a
James Eagle <jimeagle@gmail.com>
2020-12-17 21:20:30 +0000

    Auto download deps

--- cmd/api/scripts/generate.sh
+++ cmd/api/scripts/generate.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+files=(
+  "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+)
+
+for i in "${files[@]}"; do
+  if [ ! -f $(which $(basename $i)) ]; then
+    go get -u "$i"
+  fi
+done
+
 oapi-codegen \
   -o ./generated/generated.go \
   -generate types,chi-server,spec \
