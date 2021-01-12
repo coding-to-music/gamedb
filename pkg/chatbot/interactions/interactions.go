1f1658ed0404235fdd522eb95d4d652823604ae0
James Eagle <jimeagle@gmail.com>
2021-01-12 21:21:58 +0000

    Comments

--- pkg/chatbot/interactions/interactions.go
+++ pkg/chatbot/interactions/interactions.go
@@ -16,13 +16,13 @@ type Interaction struct {
 	ID            string              `json:"id"`             // no need to set
 	ApplicationID string              `json:"application_id"` // no need to set
 	Name          string              `json:"name"`           // ^[\w-]{3,32}$
-	Description   string              `json:"description"`    //
+	Description   string              `json:"description"`    // 1-100 character description
 	Options       []InteractionOption `json:"options"`        //
 }
 
 type InteractionOption struct {
 	Name        string              `json:"name"`              // ^[\w-]{1,32}$
-	Description string              `json:"description"`       //
+	Description string              `json:"description"`       // 1-100 character description
 	Type        int                 `json:"type"`              //
 	Required    bool                `json:"required"`          //
 	Choices     []InteractionChoice `json:"choices,omitempty"` //
