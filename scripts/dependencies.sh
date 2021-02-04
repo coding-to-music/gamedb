8195536f23a9814c3fdf9f5936410493231b9c64
James Eagle <jimeagle@gmail.com>
2021-02-04 20:03:11 +0000

    Update deps scripts

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+#cd ~/
+#go get -u github.com/loov/goda
+#go get -u github.com/goccy/go-graphviz
+#brew install graphviz
+#cd -
+
 goda graph ../...:root | dot -Tsvg -o dependencies.svg
