eea3b3ee0385751253e0742d533e93847a80a536
James Eagle <jimeagle@gmail.com>
2021-03-01 20:05:59 +0000

    Move chatbot api code

--- cmd/chatbot/main.go
+++ cmd/chatbot/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"strings"
 	"time"
 
 	"github.com/Jleagle/rate-limit-go"
@@ -81,7 +83,80 @@ func main() {
 	)
 }
 
-func updateGuildsCount() {
+func refreshCommands(session *discordgo.Session) error {
+
+	apiCommands, err := session.ApplicationCommands(config.DiscordBotClientID, config.DiscordGuildID)
+	if err != nil {
+		return err
+	}
+
+	// Delete removed commands
+	for _, apiCommand := range apiCommands {
+		if _, ok := chatbot.CommandCache[apiCommand.Name]; !ok {
+
+			log.Info("Deleting dommand", zap.String("id", apiCommand.Name))
+			err = session.ApplicationCommandDelete(config.DiscordBotClientID, config.DiscordGuildID, apiCommand.ID)
+			if err != nil {
+				log.Err("Deleting command", zap.Error(err))
+			}
+		}
+	}
+
+	// Update updated commands
+	for _, apiCommand := range apiCommands {
+		if localCommand, ok := chatbot.CommandCache[apiCommand.Name]; ok {
+
+			if apiCommand.Options == nil {
+				apiCommand.Options = []*discordgo.ApplicationCommandOption{}
+			}
+
+			b1, _ := json.Marshal(apiCommand.Options)
+			b2, _ := json.Marshal(localCommand.Slash())
+			if string(b1) != string(b2) {
+
+				log.Info("Updating command", zap.String("id", localCommand.ID()))
+				command := &discordgo.ApplicationCommand{
+					Name:        localCommand.ID(),
+					Description: strings.ToUpper(string(localCommand.Type())) + ": " + localCommand.Description(),
+					Options:     localCommand.Slash(),
+				}
+				_, err = session.ApplicationCommandCreate(config.DiscordBotClientID, config.DiscordGuildID, command)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// Add missing commands
+	for k, localCommand := range chatbot.CommandCache {
+		func() {
+
+			// Check if already exists
+			for _, apiCommand := range apiCommands {
+				if apiCommand.Name == k {
+					return
+				}
+			}
+
+			log.Info("Adding command", zap.String("id", localCommand.ID()))
+			command := &discordgo.ApplicationCommand{
+				Name:        localCommand.ID(),
+				Description: strings.ToUpper(string(localCommand.Type())) + ": " + localCommand.Description(),
+				Options:     localCommand.Slash(),
+			}
+			_, err = session.ApplicationCommandCreate(config.DiscordBotClientID, config.DiscordGuildID, command)
+			if err != nil {
+				log.Err("Adding command", zap.String("id", localCommand.ID()))
+				return
+			}
+		}()
+	}
+
+	return nil
+}
+
+func updateGuildsCount(session *discordgo.Session) {
 
 	if config.IsProd() {
 		for {
