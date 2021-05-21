f0c6fac54512a656d3ba0ba529684b46d74c32fb
James Eagle <jimeagle@gmail.com>
2021-05-21 12:43:11 +0100

    Check for empty ldflags

--- cmd/frontend/handlers/commits.go
+++ cmd/frontend/handlers/commits.go
@@ -71,9 +71,12 @@ func commitsAjaxHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//
-	commitsCount, err := strconv.Atoi(ldflags.CommitCount)
-	if err != nil {
-		log.ErrS(err)
+	var commitsCount int
+	if ldflags.CommitCount != "" {
+		commitsCount, err = strconv.Atoi(ldflags.CommitCount)
+		if err != nil {
+			log.ErrS(err)
+		}
 	}
 
 	var response = datatable.NewDataTablesResponse(r, query, int64(commitsCount), int64(commitsCount), nil)
